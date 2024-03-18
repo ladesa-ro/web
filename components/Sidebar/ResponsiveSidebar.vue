f2e954b6b564a54576637d21b4a13ef45f24f361
escuderoDP <danilo.p.escudero@gmail.com>
2024-03-18 12:56:47 -0400

    feat
    Criação de um menu responsivo de acordo com o tamanho da tela, que usa os componentes WebSidebar.vue e MobileSidebar.vue.

---
+++ components/Sidebar/ResponsiveSidebar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useMonitorSize } from '~/composables/monitor-size';
+import Mobile from './MobileSidebar.vue';
+import Web from './WebSidebar.vue';
+
+const sizes = useMonitorSize();
+
+</script>
+
+<template>
+    <Mobile v-if="sizes.isMobile.value" />
+    <Web v-else />
+</template>
