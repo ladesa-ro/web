64f6c6592866771749f2cbac59977dbc95217d21
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-19 13:28:07 -0400

    feat: SearchBar: use UITextFieldBase

--- components/UI/SearchBar/SearchBar.vue
+++ components/UI/SearchBar/SearchBar.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="searchField">
-    <v-text-field hide-details base-color="#9ab69e" color="#00d047" persistent-hint label="Pesquisar"
-      placeholder="Digite Aqui" variant="outlined" persistent-placeholder clearable append-inner-icon="mdi-magnify"
-      v-bind="$attrs" />
+    <UITextFieldBase clearable label="Pesquisar" placeholder="Digite Aqui" append-inner-icon="mdi-magnify" />
   </div>
 </template>
 
@@ -11,31 +9,5 @@
   display: block;
   max-width: 480px;
   width: 100%;
-}
-
-.searchField .v-field {
-  border-radius: 8px;
-}
-
-.searchField .v-field--variant-outlined.v-field--focused .v-field__outline,
-.searchField .v-field .v-field__outline {
-  --v-field-border-width: 3px !important;
-  --v-field-border-opacity: 1 !important;
-}
-
-.searchField .v-field__input {
-  min-height: 48px;
-  padding: 4px 16px;
-  font-size: 14px;
-  font-weight: 550;
-}
-
-.searchField .v-label {
-  --v-medium-emphasis-opacity: 1 !important;
-}
-
-.searchField .v-field__input::placeholder {
-  color: #c8d7ca;
-  --v-disabled-opacity: 1 !important;
 }
 </style>
