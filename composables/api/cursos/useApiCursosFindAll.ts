ab099f232c9b60edd560979178d1583c902380e3
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-21 15:19:20 -0400

    feat: useApiCursosFindAll: searchTerm: MaybeRef<string>

--- composables/api/cursos/useApiCursosFindAll.ts
+++ composables/api/cursos/useApiCursosFindAll.ts
@@ -1,17 +1,16 @@
 import { useQuery } from "@tanstack/vue-query";
 import { CursosService } from "../../../infrastructure/api/generated";
 
-export const useApiCursosFindAll = async (searchTerm: string) => {
+export const useApiCursosFindAll = async (searchTerm: MaybeRef<string>) => {
   const query = useQuery({
     queryKey: ["cursos", searchTerm],
-    
+
     queryFn: async () => {
-      return CursosService.cursoControllerCursoFindAll( undefined, undefined, searchTerm );
+      return CursosService.cursoControllerCursoFindAll(undefined, undefined, unref(searchTerm));
     },
   });
 
   const cursos = computed(() => unref(query.data)?.data ?? []);
-
 
   await query.suspense();
 
