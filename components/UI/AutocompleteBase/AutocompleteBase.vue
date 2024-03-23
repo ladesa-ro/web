08821ab2a06799ac1aefeeaa4eeb100acd698990
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-03-23 13:18:11 -0400

    fix: text field and autocomplete: active colors

--- components/UI/AutocompleteBase/AutocompleteBase.vue
+++ components/UI/AutocompleteBase/AutocompleteBase.vue
@@ -1,24 +1,64 @@
 <template>
-  <v-autocomplete
-  label="Autocomplete"
-  variant="outlined"
-  />
+  <div class="autoCompleteField">
+    <v-autocomplete
+      hide-details="auto"
+      base-color="#9ab69e"
+      color="#00d047"
+      clearable
+      persistent-placeholder
+      variant="outlined"
+      v-bind="$attrs"
+    />
+  </div>
 </template>
 
-<script lang="ts" setup>
-
-import { defineProps } from 'vue';
-
-
-
-defineProps({
-  listItems: Array,
-});
-
-</script>
-
-
-
 <style>
+.v-input__details {
+  text-align: start !important;
+  padding-left: 0px !important;
+}
 
+.autoCompleteField {
+  display: block;
+  width: 100%;
+}
+
+.autoCompleteField .v-field--variant-outlined .v-field__outline__start {
+  flex-basis: 22px;
+}
+
+.autoCompleteField .v-field {
+  border-radius: 8px;
+}
+
+.autoCompleteField .v-field__input {
+  min-height: 48px;
+  padding: 4px 16px;
+  font-size: 14px;
+  font-weight: 550;
+  color: black;
+}
+
+.autoCompleteField .v-label {
+  --v-medium-emphasis-opacity: 1 !important;
+}
+
+.v-label.v-field-label.v-field-label--floating {
+  font-weight: 600;
+}
+
+.autoCompleteField .v-field__input::placeholder {
+  color: #c8d7ca;
+  --v-disabled-opacity: 1 !important;
+}
+
+.autoCompleteField .v-field .v-field__outline {
+  --v-field-border-width: 3px !important;
+  --v-field-border-opacity: 1 !important;
+}
+
+.autoCompleteField .v-field .v-field__outline {
+  --v-border-color: #9ab69e;
+  color: #9ab69e;
+}
 </style>
--- components/VV/Autocomplete.vue
+++ components/VV/Autocomplete.vue
@@ -1,29 +1,28 @@
 <template>
-  <div class="textField">
-    <v-autocomplete
-    v-model="inputValue"
-    :error-messages="errorMessage ? [errorMessage] : []"
-    hide-details="auto"
-    clearable
-    persistent-placeholder
-    @input="handleChange"
-    @blur="handleBlur"
-    :name="name" 
-    :id="name"
-    variant="outlined"
-    v-bind="$attrs"
-  />
+  <div class="autoCompleteField">
+    <UIAutocompleteBase
+      v-model="inputValue"
+      :error-messages="errorMessage ? [errorMessage] : []"
+      hide-details="auto"
+      clearable
+      persistent-placeholder
+      @input="handleChange"
+      @blur="handleBlur"
+      :name="name"
+      :id="name"
+      v-bind="$attrs"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, toRef } from 'vue';
-import { useField } from 'vee-validate';
+import { useField } from "vee-validate";
+import { defineProps, toRef } from "vue";
 
 const props = defineProps({
   value: {
     type: String,
-    default: '',
+    default: "",
   },
   name: {
     type: String,
@@ -31,7 +30,7 @@ const props = defineProps({
   },
 });
 
-const name = toRef(props, 'name');
+const name = toRef(props, "name");
 
 const {
   value: inputValue,
@@ -39,61 +38,8 @@ const {
   handleBlur,
   handleChange,
   meta,
-} = useField(name.value, undefined, 
-  {
-    initialValue: props.value,
-    validateOnValueUpdate: false,
-  }
-);
+} = useField(name.value, undefined, {
+  initialValue: props.value,
+  validateOnValueUpdate: false,
+});
 </script>
-
-<style>
-.v-input__details {
-  text-align: start !important;
-  padding-left: 0px !important;
-}
-
-  .textField {
-    display: block;
-    width: 100%;
-  }
-  
-  .textField .v-field--variant-outlined .v-field__outline__start {
-    flex-basis: 22px;
-  }
-  
-  .textField .v-field {
-    border-radius: 8px;
-  }
-  
-  .textField .v-field--variant-outlined.v-field--focused .v-field__outline,
-  .textField .v-field .v-field__outline {
-    --v-field-border-width: 3px !important;
-    --v-field-border-opacity: 1 !important;
-    --v-border-color: #9ab69e !important;
-    color: #9ab69e !important;
-  }
-
-
-  
-  .textField .v-field__input {
-    min-height: 48px;
-    padding: 4px 16px;
-    font-size: 14px;
-    font-weight: 550;
-    color: #9ab69e;
-  }
-  
-  .textField .v-label {
-    --v-medium-emphasis-opacity: 1 !important;
-  }
-  
-  .v-label.v-field-label.v-field-label--floating {
-    font-weight: 600;
-  }
-  
-  .textField .v-field__input::placeholder {
-    color: #c8d7ca;
-    --v-disabled-opacity: 1 !important;
-  }
-</style>
