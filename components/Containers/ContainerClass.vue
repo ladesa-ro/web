d6ac96a3b27eca36c5ee36b1acf9e8cc4982ea75
Sr-Iury <iury.silvafim@gmail.com>
2024-03-27 17:45:48 -0300

    Atualizado para Vue3
    feat: Atualizado para o Vue3 todas as páginas anteriores que estavam em Vue2

--- components/Containers/ContainerClass.vue
+++ components/Containers/ContainerClass.vue
@@ -1,9 +1,17 @@
 <script setup>
 const props = defineProps({
-  imgSr: String,
-  sala: String,
-  curso: String,
-  turno: String,
+  imgSr:{
+    type:String
+  },
+  turno:{
+    type:String
+  },
+  curso:{
+    type:String
+  },
+  sala:{
+    type:String
+  }
 });
 
 const { imgSr, sala, curso, turno } = props;
--- components/Pages/DashboardDisciplinas/DashboardDisciplinas.vue
+++ components/Pages/DashboardDisciplinas/DashboardDisciplinas.vue
@@ -1,5 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import caminhoDaImagem from "~/assets/Foto.png";
+import SavedModal from "~/components/Modais/SavedModal.vue";
+import adicionar from "~/componentes/adicionar.vue";
+import ButtonAdd from "~/components/UI/ButtonAdd/ButtonAdd.vue";
+import ContainerDiscipline from "~/components/Containers/ContainerDiscipline.vue";
+
+const mensagemDoPai = ref("Linguagem e Lógica de Programação");
+const abre = "Abreviação: L. L. Prog.";
+const niv = "Modalidade: Técnico Integrado";
+
+const caminhoDaImagem2 = "https://tse1.mm.bing.net/th?id=OIP.lKoTmKPpWT-qSlTLmlIwwgHaEK&pid=Api&P=0&h=180";
+const mensagemDoPai2 = ref("Matemática");
+const abre2 = "Abreviação: Mat.";
+const niv2 = "Modalidade: Técnico Integrado";
+
+const caminhoDaImagem3 = "https://tse4.mm.bing.net/th?id=OIP.-eVHxNSXqiqHdFy8tlPlJAHaFj&pid=Api&P=0&h=180";
+const mensagemDoPai3 = ref("Português");
+const abre3 = "Abreviação: Port.";
+const niv3 = "Modalidade: Técnico Integrado";
+
+const msg = "Digite aqui.";
+const showModal = ref(false);
+</script>
 <template>
-  <search-bar class="search" :msge="msg"></search-bar>
+  <SearchBar value="Pesquisar" />
   <ContainerDiscipline
     class="p1"
     :imagemSrc="caminhoDaImagem"
@@ -31,47 +56,6 @@
     </button>
   </div>
 </template>
-
-<script>
-import caminhoDaImagem from "~/assets/Foto.png";
-import SavedModal from "~/components/Modais/SavedModal.vue";
-import SearchBar from "~/components/SearchBar.vue";
-import adicionar from "~/componentes/adicionar.vue";
-import ButtonAdd from "~/components/UI/ButtonAdd/ButtonAdd.vue";
-import ContainerDiscipline from "~/components/Containers/ContainerDiscipline.vue";
-export default {
-  components: {
-    ContainerDiscipline,
-    SearchBar,
-    adicionar,
-    SavedModal,
-    ButtonAdd
-  },
-  data() {
-    return {
-      caminhoDaImagem: caminhoDaImagem,
-      mensagemDoPai: "Linguagem e Lógica de Programação", // Defina o valor da prop aqui
-      abre: "Abreviação: L. L. Prog.",
-      niv: "Modalidade: Técnico Integrado",
-
-      caminhoDaImagem2:
-        "https://tse1.mm.bing.net/th?id=OIP.lKoTmKPpWT-qSlTLmlIwwgHaEK&pid=Api&P=0&h=180",
-      mensagemDoPai2: "Matemática", // Defina o valor da prop aqui
-      abre2: "Abreviação: Mat.",
-      niv2: "Modalidade: Técnico Integrado",
-
-      caminhoDaImagem3:
-        "https://tse4.mm.bing.net/th?id=OIP.-eVHxNSXqiqHdFy8tlPlJAHaFj&pid=Api&P=0&h=180",
-      mensagemDoPai3: "Português", // Defina o valor da prop aqui
-      abre3: "Abreviação: Port.",
-      niv3: "Modalidade: Técnico Integrado",
-
-      msg: "Digite aqui.",
-      showModal: false,
-    };
-  },
-};
-</script>
 
 <style>
 .t {
