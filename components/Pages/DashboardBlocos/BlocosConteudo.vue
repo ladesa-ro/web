ebfe799e5daa98b3b1cadaa090eeb18209a8ad53
GabrielReds13 <speedckprod4007@gmail.com>
2024-04-17 16:55:35 -0400

    Fix: Rename Classes

--- components/Pages/DashboardBlocos/BlocosConteudo.vue
+++ components/Pages/DashboardBlocos/BlocosConteudo.vue
@@ -1,53 +1,56 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref } from 'vue';
 
 const show = ref(false);
 
-let searchBarText = ref("");
+let searchBarText = ref('');
 </script>
 
 <template>
-  <v-container>
-    <div class="container">
-      <div>
-        <div class="container-header px-3">
-          <UISearchBar :value="searchBarText" @update:value="searchBarText = $event" />
+	<v-container>
+		<div class="container">
+			<div>
+				<div class="container-header px-3">
+					<UISearchBar
+						:value="searchBarText"
+						@update:value="searchBarText = $event"
+					/>
 
-          <div class="container-header-actions">
-            <PagesDashboardBlocosModalCadastrarBloco />
-          </div>
-        </div>
+					<div class="container-header-actions">
+						<PagesDashboardBlocosModalCadastrarBloco />
+					</div>
+				</div>
 
-        <div class="cardCoursesGeneral">
-          <PagesDashboardBlocosLista :searchBarText="searchBarText" />
-        </div>
-      </div>
-    </div>
-  </v-container>
+				<div class="-card-bloco-geral">
+					<PagesDashboardBlocosLista :searchBarText="searchBarText" />
+				</div>
+			</div>
+		</div>
+	</v-container>
 </template>
 
 <style scoped>
 .container {
-  margin: 0 auto;
-  max-width: 89%;
+	margin: 0 auto;
+	max-width: 89%;
 }
 
 .container-header {
-  padding: 50px 0;
+	padding: 50px 0;
 
-  margin: 0 auto;
-  display: flex;
+	margin: 0 auto;
+	display: flex;
 
-  gap: 1rem;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
+	gap: 1rem;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
 }
 
 .container-header-actions {
-  flex-shrink: 0;
+	flex-shrink: 0;
 
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
 }
 </style>
