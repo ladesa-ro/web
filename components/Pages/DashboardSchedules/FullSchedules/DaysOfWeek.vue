5273337cc19973295a34cc4fdf0b2b46d7d9e563
Kauan <daluzmarkes@gmail.com>
2024-05-04 21:14:01 -0400

    feat: Shift Schedule

--- components/Pages/DashboardSchedules/FullSchedules/DaysOfWeek.vue
+++ components/Pages/DashboardSchedules/FullSchedules/DaysOfWeek.vue
@@ -2,23 +2,35 @@
 <script setup></script>
 <template>
 	<v-container class="flex gap-2 m-0 p-0">
-		<div class="bg-[#118D3B] text-white py-3 rounded-t-lg">
-			<span class="px-6 text-nowrap">Segunda Feira - 06/03</span>
+		<div
+			class="bg-[#118D3B] text-white w-[300px] text-center content-center rounded-t-lg"
+		>
+			<span class="text-nowrap">Segunda Feira - 06/03</span>
 		</div>
-		<div class="bg-[#118D3B] text-white py-3 rounded-t-lg">
-			<span class="px-6 text-nowrap">Terça Feira - 06/03</span>
+		<div
+			class="bg-[#118D3B] text-white w-[280px] text-center content-center rounded-t-lg"
+		>
+			<span class="text-nowrap">Terça Feira - 06/03</span>
 		</div>
-		<div class="bg-[#118D3B] text-white py-3 rounded-t-lg">
-			<span class="px-6 text-nowrap">Quarta Feira - 06/03</span>
+		<div
+			class="bg-[#118D3B] text-white w-[280px] text-center content-center rounded-t-lg"
+		>
+			<span class="text-nowrap">Quarta Feira - 06/03</span>
 		</div>
-		<div class="bg-[#118D3B] text-white py-3 rounded-t-lg">
-			<span class="px-6 text-nowrap">Quinta Feira - 06/03</span>
+		<div
+			class="bg-[#118D3B] text-white w-[280px] text-center content-center rounded-t-lg"
+		>
+			<span class="text-nowrap">Quinta Feira - 06/03</span>
 		</div>
-		<div class="bg-[#118D3B] text-white py-3 rounded-t-lg">
-			<span class="px-6 text-nowrap">Sexta Feira - 06/03</span>
+		<div
+			class="bg-[#118D3B] text-white w-[280px] text-center content-center rounded-t-lg"
+		>
+			<span class="text-nowrap">Sexta Feira - 06/03</span>
 		</div>
-		<div class="bg-[#118D3B] text-white py-3 rounded-t-lg">
-			<span class="px-6 text-nowrap">Sábado - 30/12</span>
+		<div
+			class="bg-[#118D3B] text-white w-[280px] h-[50px] text-center content-center rounded-t-lg"
+		>
+			<span class="text-nowrap">Sábado - 30/12</span>
 		</div>
 	</v-container>
 </template>
