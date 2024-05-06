29df3b3e56becdfd6b4e07bee2eddba55c244def
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-06 10:22:26 -0400

    feat: enhance sidebar

---
+++ components/Sidebar/SidebarItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { ISidebarItem } from './ISidebarItem';
+
+const props = defineProps({
+	item: {
+		type: Object,
+		required: true,
+	},
+}) as { item: ISidebarItem };
+
+const { item } = toRefs(props);
+</script>
+
+<template>
+	<SidebarItemLink v-if="item.type === 'link'" :item="item" v-bind="$attrs" />
+
+	<SidebarItemGroup
+		v-if="item.type === 'group'"
+		:item="item"
+		v-bind="$attrs"
+	/>
+
+	<SidebarItemSpacer
+		v-if="item.type === 'spacer'"
+		:item="item"
+		v-bind="$attrs"
+	/>
+</template>
