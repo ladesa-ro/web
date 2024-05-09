8a7bf29a687dd547a76a36e6a7920f21d3170488
Sr-Iury <iury.silvafim@gmail.com>
2024-05-09 18:34:49 -0300

    refactor: delete folders :package:
    the Disciplines, Courses and blocks dashboards were deleted, which were no longer useful for the system

---
+++ components/Section/Ambientes/Grid/Grid.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+const props = defineProps({
+	searchBarText: String,
+});
+
+const $emit = defineEmits(['edit']);
+
+const { searchBarText } = toRefs(props);
+
+const { ambientes } = await useApiAmbientesFindAll(searchBarText);
+</script>
+
+<template>
+	<UIGrid :items="ambientes">
+		<template #item="{ item: ambiente }">
+			<UICard
+				variant="block"
+				:src="`https://luna.sisgha.com/api/ambientes/${ambiente.id}/imagem/capa?imgCapa=${ambiente.imagemCapa?.id}`"
+			>
+				<template #title>
+					{{ ambiente.nome }}
+				</template>
+
+				<template #actions>
+          <SectionAmbientesModal :editId="ambiente.id" />
+				</template>
+
+				<UICardLine>
+					{{ ambiente.bloco.nome }}
+				</UICardLine>
+				<UICardLine>
+					Capacidade: {{ ambiente.capacidade }} pessoas
+				</UICardLine>
+			</UICard>
+		</template>
+	</UIGrid>
+</template>
