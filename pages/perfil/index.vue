600040321c7920fc5d21b2ef396564c413cb6b1e
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-09 14:12:26 -0400

    fix: usages of useTitle

--- pages/perfil/index.vue
+++ pages/perfil/index.vue
@@ -1,5 +1,7 @@
-<script setup>
-useAppHeadTitle('Perfil');
+<script lang="ts" setup>
+import { HeadTitleContext } from '~/infrastructure/HeadTitleContext';
+
+useTitle(HeadTitleContext.DASHBOARD, 'Perfil');
 </script>
 <template>
 	<div>
