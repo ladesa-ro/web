abdddb1535fd843a7e758e6d7f3346ad0031abab
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-10 15:42:45 -0400

    refactor(components/Pages/DashboardSchedules/FullSchedules/Class): code update :package:
    formatting changes, such as increasing font-size and line-height.

--- components/Pages/DashboardSchedules/FullSchedules/Class.vue
+++ components/Pages/DashboardSchedules/FullSchedules/Class.vue
@@ -7,14 +7,14 @@
 </script>
 
 <template>
-<div class="flex flex-row items-center justify-between border-solid border-[1.5px] border-[#118D3B] rounded-lg w-[692px] h-[75px] px-5 py-[10px] text-xs">
+<div class="flex flex-row items-center justify-between border-solid border-[1.5px] border-[#118D3B] rounded-lg px-5 py-3">
   <section class="flex flex-col justify-between">
-    <h1 class="leading-[18px] font-semibold">Linguagem de Programação - 2° A Informática</h1>
-    <p class="font-normal text-[11px]">Ambiente: Sala 20</p>
-    <p class="font-normal text-[11px]">Horário: 13:50 - 14:40</p>
+    <h1 class="font-[600]">Linguagem de Programação - 2° A Informática</h1> <!--leading-[18px] text-[12px] font-[600]-->
+    <p>Ambiente: Sala 20</p> <!--leading-[17px] text-[11px] font-[600]-->
+    <p>Horário: 13:50 - 14:40</p>
   </section>
 
-  <div class=""></div>
+  <div></div>
 </div>
 </template>
 
