48ce77e3ee44c8ba847aaf6914ca482b69e21458
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-10 15:47:50 -0400

    refactor(components/Pages/DashboardSchedules/FullSchedules/Class): increasing component border :package:

--- components/Pages/DashboardSchedules/FullSchedules/Class.vue
+++ components/Pages/DashboardSchedules/FullSchedules/Class.vue
@@ -7,7 +7,7 @@
 </script>
 
 <template>
-<div class="flex flex-row items-center justify-between border-solid border-[1.5px] border-[#118D3B] rounded-lg px-5 py-3">
+<div class="flex flex-row items-center justify-between border-2 border-[#118D3B] rounded-lg px-5 py-3">
   <section class="flex flex-col justify-between">
     <h1 class="font-[600]">Linguagem de Programação - 2° A Informática</h1> <!--leading-[18px] text-[12px] font-[600]-->
     <p>Ambiente: Sala 20</p> <!--leading-[17px] text-[11px] font-[600]-->
