d214ddde1a8b5d9b6716dd783c88d792989b1c9d
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-10 15:06:46 -0400

    feat: add page HorarioDiario :sparkles:
    This page has the HeaderSchedule, ListagemDaySquare and Class components.

---
+++ pages/HorarioDiario.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+<div class="flex flex-col gap-[30px]">
+    <PagesDashboardSchedulesFullSchedulesHeaderSchedule/>
+    
+    <PagesDashboardSchedulesFullSchedulesListagemDaySquare/>
+
+    <v-container class="flex flex-col gap-5">
+        <PagesDashboardSchedulesFullSchedulesClass/>
+        <PagesDashboardSchedulesFullSchedulesClass/>
+        <PagesDashboardSchedulesFullSchedulesClass/>
+    </v-container>
+</div>
+</template>
+
+<style scoped>
+</style>
