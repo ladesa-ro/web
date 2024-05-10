c4a85764efef8a90217f1486e4143e968e15c4e1
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-10 14:37:29 -0400

    feat: add component ListagemDaySquare :sparkles:

---
+++ components/Pages/DashboardSchedules/FullSchedules/ListagemDaySquare.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <v-container class="flex flex-row justify-between">
+      <PagesDashboardSchedulesFullSchedulesDaySquare id="today"/>
+      <PagesDashboardSchedulesFullSchedulesDaySquare/>
+      <PagesDashboardSchedulesFullSchedulesDaySquare/>
+      <PagesDashboardSchedulesFullSchedulesDaySquare/>
+      <PagesDashboardSchedulesFullSchedulesDaySquare/>
+      <PagesDashboardSchedulesFullSchedulesDaySquare/>
+  </v-container>
+</template>
+
+<style scoped></style>
