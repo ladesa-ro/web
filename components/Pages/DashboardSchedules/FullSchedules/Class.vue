9d6beff41222f2520391989ef374dbca4783c4bb
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-10 14:57:44 -0400

    feat: add component Class :sparkles:

---
+++ components/Pages/DashboardSchedules/FullSchedules/Class.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import { ref } from 'vue';
+// const subject = ref('Linguagem de Programação');
+// const _class = ref('2° A Informática');
+// const place = ref('Sala 20');
+// const time = ref('13:50 - 14:40');
+</script>
+
+<template>
+<div class="flex flex-row items-center justify-between border-solid border-[1.5px] border-[#118D3B] rounded-lg w-[692px] h-[75px] px-5 py-[10px] text-xs">
+  <section class="flex flex-col justify-between">
+    <h1 class="leading-[18px] font-semibold">Linguagem de Programação - 2° A Informática</h1>
+    <p class="font-normal text-[11px]">Ambiente: Sala 20</p>
+    <p class="font-normal text-[11px]">Horário: 13:50 - 14:40</p>
+  </section>
+
+  <div class=""></div>
+</div>
+</template>
+
+<style scoped></style>
