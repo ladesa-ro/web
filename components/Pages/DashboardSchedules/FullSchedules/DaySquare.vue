5c9635ddbbf0a7e14632d2a98efdbd1d865a486f
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-10 17:52:17 -0400

    fix(components/Pages/DashboardSchedules/FullSchedules/DaySquare): add max-width
    add max-width to component to fix and avoid bugs

--- components/Pages/DashboardSchedules/FullSchedules/DaySquare.vue
+++ components/Pages/DashboardSchedules/FullSchedules/DaySquare.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-<div class="flex flex-col justify-center items-center gap-2 rounded-[10px] border-2 border-[#118D3B] p-4"> <!--w-[85px] h-[75px] // w-[106.25px] h-[100px]-->
+<div class="flex flex-col justify-center items-center gap-2 rounded-[10px] border-2 border-[#118D3B] p-4 max-w-28 max-h-28"> <!--w-[85px] h-[75px] // w-[106.25px] h-[100px]-->
     <p class="border-b-2 border-b-[#118D3B] p-0 pb-[5px] font-[600] text-[#118D3B]">Segunda</p>
     <p class="font-[600] text-[#118D3B]">06/03</p>
 </div>
