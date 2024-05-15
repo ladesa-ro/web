23e1ddcc30976754edf857ce54be28270c535ef2
Kauan <daluzmarkes@gmail.com>
2024-05-14 22:01:23 -0400

    refactor: refactoring the code user modal

--- components/Pages/DashboardUsers/Forms/Disponibilidade.vue
+++ components/Pages/DashboardUsers/Forms/Disponibilidade.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import IconArrow from '~/components/Icons/Arrow/IconArrow.vue';
 const selectedOptions = ref();
 
 const options = [
@@ -10,6 +11,41 @@ const options = [
 	{ value: 'Quinta' },
 	{ value: 'Sexta' },
 	{ value: 'Sábado' },
+];
+const turnos = [
+	{
+		nome: 'Matutino',
+		horarios: [
+			{ hora: '07:30', tipo: 'aula' },
+			{ hora: '08:20', tipo: 'aula' },
+			{ hora: '09:10', tipo: 'aula' },
+			{ hora: '10:00', tipo: 'intervalo' },
+			{ hora: '10:20', tipo: 'aula' },
+			{ hora: '11:10', tipo: 'aula' },
+		],
+	},
+	{
+		nome: 'Vespertino',
+		horarios: [
+			{ hora: '13:00', tipo: 'aula' },
+			{ hora: '13:50', tipo: 'aula' },
+			{ hora: '14:40', tipo: 'aula' },
+			{ hora: '15:30', tipo: 'intervalo' },
+			{ hora: '15:50', tipo: 'aula' },
+			{ hora: '16:40', tipo: 'aula' },
+		],
+	},
+	{
+		nome: 'Noturno',
+		horarios: [
+			{ hora: '19:00', tipo: 'aula' },
+			{ hora: '19:50', tipo: 'aula' },
+			{ hora: '20:40', tipo: 'aula' },
+			{ hora: '21:30', tipo: 'intervalo' },
+			{ hora: '21:50', tipo: 'aula' },
+			{ hora: '22:40', tipo: 'aula' },
+		],
+	},
 ];
 
 selectedOptions.value = options[1];
@@ -26,80 +62,45 @@ const changeDay = (delta) => {
 
 <template>
 	<div class="flex justify-between items-center mt-5">
-			<!-- <IconBeforeDays class="cursor-pointer" @click="changeDay(-1)" /> -->
+		<IconArrow class="cursor-pointer" @click="changeDay(-1)" />
 		<div>
 			<span class="font-medium">
 				{{ selectedOptions?.value }}
 			</span>
 		</div>
-			<!-- <IconAfterDays class="cursor-pointer" @click="changeDay(1)" /> -->
+		<IconArrow class="cursor-pointer rotate-180" @click="changeDay(1)" />
 	</div>
 
-<v-checkbox
-	hide-details
-	density="compact"
-	color="success"
-	label="Manhã"
-	v-for="i in 7"
-/>
+	<section class="flex justify-between mt-10">
+		<div>
+			<span>Matutino</span>
+			<v-checkbox
+				hide-details
+				density="compact"
+				color="success"
+				label="Manhã"
+				v-for="i in 7"
+			/>
+		</div>
+		<div>
+			<span>Vespertino</span>
+			<v-checkbox
+				hide-details
+				density="compact"
+				color="success"
+				label="Manhã"
+				v-for="i in 7"
+			/>
+		</div>
+		<div>
+			<span>Noturno</span>
+			<v-checkbox
+				hide-details
+				density="compact"
+				color="success"
+				label="Manhã"
+				v-for="i in 7"
+			/>
+		</div>
+	</section>
 </template>
-
-<style>
-.initmodal2,
-.contentModal {
-	display: flex;
-	align-items: center;
-	gap: 50px;
-	display: flex;
-	align-items: center;
-	gap: 50px;
-}
-
-.iconWrapper {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	margin-top: 30px;
-	margin-left: 60px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	margin-top: 30px;
-	margin-left: 60px;
-}
-
-.iconB,
-.iconA {
-	cursor: pointer;
-	cursor: pointer;
-}
-
-.iconA {
-	margin-left: 70px;
-	margin-left: 70px;
-}
-.contentModal {
-	margin-top: 40px;
-	margin-top: 40px;
-}
-
-.elementHours {
-	content: '';
-	height: 2px;
-	width: auto;
-	background-color: #9ab69e;
-	margin-top: 2px;
-	margin-bottom: 8px;
-	content: '';
-	height: 2px;
-	width: auto;
-	background-color: #9ab69e;
-	margin-top: 2px;
-	margin-bottom: 8px;
-}
-
-.hoursMorning {
-	margin-left: 10px;
-	margin-left: 10px;
-}
-</style>
