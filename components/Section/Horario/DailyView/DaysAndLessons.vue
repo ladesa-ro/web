6b597483d69ccdd1ef9904f8b78370e5a893cc09
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-05-20 23:27:50 -0400

    feat: add DaysAndLessons component :sparkles:

---
+++ components/Section/Horario/DailyView/DaysAndLessons.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { ILesson } from '~/components/Section/Horario/DailyView/IDailyView';
+
+const genericLesson: ILesson = {
+	discipline: 'Linguagem de Programação',
+	class: '2° A Informática',
+	environment: 'Sala 20',
+	startsAt: '13:50',
+	endsAt: '14:40',
+};
+
+const lessons: ILesson[] = [
+	genericLesson,
+	genericLesson,
+	genericLesson,
+	genericLesson,
+	genericLesson,
+];
+
+const activeIndex = 2;
+
+function getVariantForIndex(index: number) {
+	if (index < activeIndex) return 'completed';
+	if (index === activeIndex) return 'active';
+	return;
+}
+</script>
+
+<template>
+<SectionHorarioDailyViewListagemDaySquare />
+
+<v-container class="flex flex-col gap-5">
+    <SectionHorarioDailyViewLesson
+        v-for="(lesson, index) in lessons"
+        :lesson="lesson"
+        :variant="getVariantForIndex(index)"
+    />
+</v-container>
+</template>
+
+<style scoped></style>
--- pages/HorarioDiario.vue
+++ pages/HorarioDiario.vue
@@ -1,45 +1,10 @@
-<script setup lang="ts">
-import { LazySectionHorarioDailyViewLesson } from '#build/components';
-import type { ILesson } from '~/components/Section/Horario/DailyView/IDailyView';
-
-const genericLesson: ILesson = {
-	discipline: 'Linguagem de Programação',
-	class: '2° A Informática',
-	environment: 'Sala 20',
-	startsAt: '13:50',
-	endsAt: '14:40',
-};
-
-const lessons: ILesson[] = [
-	genericLesson,
-	genericLesson,
-	genericLesson, 
-	genericLesson, 
-	genericLesson, 
-];
-
-const activeIndex = 2;
-
-function getVariantForIndex(index: number) {
-	if (index < activeIndex) return 'completed';
-	if (index === activeIndex) return 'active';
-	return;
-}
-</script>
+<script setup></script>
 
 <template>
 	<div class="flex flex-col gap-[15px]">
 		<SectionHorarioHeaderSchedule />
-
-		<SectionHorarioDailyViewListagemDaySquare />
-
-		<v-container class="flex flex-col gap-5">
-			<LazySectionHorarioDailyViewLesson
-				v-for="(lesson, index) in lessons"
-				:lesson="lesson"
-				:variant="getVariantForIndex(index)"
-			/>
-		</v-container>
+		
+		<SectionHorarioDailyViewDaysAndLessons/>
 	</div>
 </template>
 
