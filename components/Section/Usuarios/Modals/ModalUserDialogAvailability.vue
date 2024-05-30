8dbfb2177c9f41e2d3b99521c552d796f338a5d9
Kauan <daluzmarkes@gmail.com>
2024-05-30 17:27:17 -0400

    feat: added the accordion and name by props

--- components/Section/Usuarios/Modals/ModalUserDialogAvailability.vue
+++ components/Section/Usuarios/Modals/ModalUserDialogAvailability.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import { useFormUser } from '../Form/FormUser';
-import FormUserAvailability from '../Form/FormUserAvailability.vue';
+import {
+	checkHasAtLeastOneActiveTeacherRole,
+	useFormUser,
+} from '../Form/FormUser';
+import FormUserAvailability from '../Form/FormUserAvailabilities.vue';
 
 const { values: formValues } = useFormUser();
 
-const possuiCargoDeProfessor = computed(() => {
-	return formValues.vinculos.some((vinculo) =>
-		vinculo.cargos.includes('professor')
-	);
+const hasAtLeastOneActiveTeacherRole = computed(() => {
+	return checkHasAtLeastOneActiveTeacherRole(formValues.vinculos);
 });
 </script>
 
 <template>
 	<v-card
-		v-if="possuiCargoDeProfessor"
+		v-if="hasAtLeastOneActiveTeacherRole"
 		class="dialog-style overflow-auto max-w-[500px]"
 	>
 		<FormUserAvailability close="isActive.value = false" />
