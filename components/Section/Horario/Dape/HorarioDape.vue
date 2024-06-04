03194da12b23ea883ef7d271be792d74e4423559
Kauan <daluzmarkes@gmail.com>
2024-06-04 17:58:30 -0400

    initial commit

---
+++ components/Section/Horario/Dape/HorarioDape.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-container class="border-2 border-black pa-0 h-[130px] rounded-lg mt-5 ">
+		<div class="bg-green-500 h-3/6 w-full p-0 rounded-t-[6px]">
+			<v-title> Técnico em Informática </v-title>
+		</div>
+
+		<div class="place-items-center">
+			<IconsArrowIconArrow class="rotate-[-90deg]"/>
+		</div>
+	</v-container>
+</template>
---
+++ pages/horarioDape.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HorarioDape from '~/components/Section/Horario/Dape/HorarioDape.vue';
+</script>
+
+<template>
+	<HorarioDape />
+</template>
