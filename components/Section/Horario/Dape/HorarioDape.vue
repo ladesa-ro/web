a52847775b6b423df6531fa1a5f3132fb67c77cd
Kauan <daluzmarkes@gmail.com>
2024-06-04 20:26:34 -0400

    fix: remove console.log()

--- components/Section/Horario/Dape/HorarioDape.vue
+++ components/Section/Horario/Dape/HorarioDape.vue
@@ -1,10 +1,10 @@
 <template>
-	<v-container class="border-2 border-black pa-0 h-[130px] rounded-lg mt-5 ">
+	<v-container class=" flex flex-col border-2 border-black pa-0 h-[130px] rounded-lg mt-5 items-center">
 		<div class="bg-green-500 h-3/6 w-full p-0 rounded-t-[6px]">
 			<v-title> Técnico em Informática </v-title>
 		</div>
 
-		<div class="place-items-center">
+		<div class="flex justify-center justify-items-center">
 			<IconsArrowIconArrow class="rotate-[-90deg]"/>
 		</div>
 	</v-container>
