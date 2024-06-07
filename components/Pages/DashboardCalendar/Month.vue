51496fe9fb5ea0fc616364490c297f945e1df947
GabrielReds13 <speedckprod4007@gmail.com>
2024-06-07 18:07:45 -0300

    Fix: Hover in the week

--- components/Pages/DashboardCalendar/Month.vue
+++ components/Pages/DashboardCalendar/Month.vue
@@ -10,37 +10,37 @@ dayjs.extend(isBetween);
 
 // --- Interface and types ---
 type EventData = {
-	id: string;
-	type: string;
-	startDate: dayjs.Dayjs;
-	endDate: dayjs.Dayjs;
-	color: string;
+  id: string;
+  type: string;
+  startDate: dayjs.Dayjs;
+  endDate: dayjs.Dayjs;
+  color: string;
 };
 
 type Step = Omit<EventData, 'type'> & {
-	number: number;
+  number: number;
 };
 
 type Event = Omit<EventData, 'type'> & {
-	name: string;
-	locale: string;
+  name: string;
+  locale: string;
 };
 
 type Day = {
-	num: number;
-	day: string;
-	date: dayjs.Dayjs;
-	color: string;
-	hoverActive: boolean;
+  num: number;
+  day: string;
+  date: dayjs.Dayjs;
+  color: string;
+  hoverActive: boolean;
 };
 
 // --- Props ---
 const props = defineProps({
-	year: Number,
-	toggleMonth: Boolean,
-	selectWeek: Boolean,
-	steps: Array<Step>,
-	events: Array<Event>,
+  year: Number,
+  toggleMonth: Boolean,
+  selectWeek: Boolean,
+  steps: Array<Step>,
+  events: Array<Event>,
 });
 
 //  --- Month ---
@@ -50,204 +50,190 @@ let monthNum = ref<number>(dayjs().month());
 let monthColor = ref<string>('#9ab69e');
 
 let calendarDays = {
-	daysInMonth: ref<Day[]>([]),
-	emptyDays: {
-		before: ref<number>(0),
-		after: ref<number>(0),
-	},
+  daysInMonth: ref<Day[]>([]),
+  emptyDays: {
+    before: ref<number>(0),
+    after: ref<number>(0),
+  },
 };
 
 // Functions
 
 //  --- Month num (emit value)  ---
 const emit = defineEmits<{
-	(e: 'custom:monthNum', v: number): void;
+  (e: 'custom:monthNum', v: number): void;
 }>();
 
 const handleCalling = (v: number) => {
-	if (v !== null) emit('custom:monthNum', v);
+  if (v !== null) emit('custom:monthNum', v);
 };
 
 //  --- Month data ---
 // Set days from this month
 async function setMonth(): Promise<void> {
-	try {
-		// Set empty days
-		async function setEmptyDays(): Promise<boolean> {
-			try {
-				// Calc before days
-				calendarDays.emptyDays.before.value = dayjs(
-					`${props.year!}-${monthNum.value + 1}-01`
-				).day();
-				calendarDays.emptyDays.after.value =
-					7 * 6 -
-					(calendarDays.emptyDays.before.value +
-						calendarDays.daysInMonth.value.length);
-				return true;
-			} catch (error) {
-				return false;
-			}
-		}
+  try {
+    // Set empty days
+    async function setEmptyDays(): Promise<boolean> {
+      try {
+        // Calc before days
+        calendarDays.emptyDays.before.value = dayjs(
+          `${props.year!}-${monthNum.value + 1}-01`
+        ).day();
+        calendarDays.emptyDays.after.value =
+          7 * 6 -
+          (calendarDays.emptyDays.before.value +
+            calendarDays.daysInMonth.value.length);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
 
-		// Set days
-		async function setDays(): Promise<boolean> {
-			try {
-				calendarDays.daysInMonth.value = [];
+    // Set days
+    async function setDays(): Promise<boolean> {
+      try {
+        calendarDays.daysInMonth.value = [];
 
-				// Set days
-				async function setDaysData(): Promise<boolean> {
-					try {
-						// Repeat loop
-						for (
-							let i = 0;
-							i <
-							dayjs(
-								dayjs(`${props.year!}-${monthNum.value + 1}-01`)
-							).daysInMonth();
-							i++
-						) {
-							calendarDays.daysInMonth.value.push({
-								num: i,
-								day: dayjs(
-									`${props.year!}-${monthNum.value + 1}-${i + 1}`
-								).format('dddd'),
-								date: dayjs(
-									`${props.year!}-${monthNum.value + 1}-${i + 1}`
-								),
-								color: '#9ab69e',
-								hoverActive: false,
-							});
-						}
+        // Set days
+        async function setDaysData(): Promise<boolean> {
+          try {
+            // Repeat loop
+            for (
+              let i = 0;
+              i <
+              dayjs(
+                dayjs(`${props.year!}-${monthNum.value + 1}-01`)
+              ).daysInMonth();
+              i++
+            ) {
+              calendarDays.daysInMonth.value.push({
+                num: i,
+                day: dayjs(
+                  `${props.year!}-${monthNum.value + 1}-${i + 1}`
+                ).format('dddd'),
+                date: dayjs(`${props.year!}-${monthNum.value + 1}-${i + 1}`),
+                color: '#9ab69e',
+                hoverActive: false,
+              });
+            }
 
-						return true;
-					} catch (error) {
-						return false;
-					}
-				}
+            return true;
+          } catch (error) {
+            return false;
+          }
+        }
 
-				// Set steps
-				async function setDatesColor(): Promise<boolean> {
-					try {
-						// Set steps
-						for (let i = 0; i < props.steps!.length; i++) {
-							// Check if date is between the start or end of the month
-							for (
-								let j = 0;
-								j < calendarDays.daysInMonth.value.length;
-								j++
-							) {
-								// Set start and end day color
-								if (
-									// Check if the date is between
-									dayjs(
-										calendarDays.daysInMonth.value[j].date
-									).isBetween(
-										props.steps![i].startDate,
-										props.steps![i].endDate,
-										'date',
-										'[]'
-									) === true
-								) {
-									// Set color
-									calendarDays.daysInMonth.value[j].color =
-										props.steps![i].color;
-								} else {
-								}
-							}
-						}
+        // Set steps
+        async function setDatesColor(): Promise<boolean> {
+          try {
+            // Set steps
+            for (let i = 0; i < props.steps!.length; i++) {
+              // Check if date is between the start or end of the month
+              for (let j = 0; j < calendarDays.daysInMonth.value.length; j++) {
+                // Set start and end day color
+                if (
+                  // Check if the date is between
+                  dayjs(calendarDays.daysInMonth.value[j].date).isBetween(
+                    props.steps![i].startDate,
+                    props.steps![i].endDate,
+                    'date',
+                    '[]'
+                  ) === true
+                ) {
+                  // Set color
+                  calendarDays.daysInMonth.value[j].color =
+                    props.steps![i].color;
+                } else {
+                }
+              }
+            }
 
-						// Set events
-						for (let i = 0; i < props.events!.length; i++) {
-							// Check if date is between the start or end of the month
-							for (
-								let j = 0;
-								j < calendarDays.daysInMonth.value.length;
-								j++
-							) {
-								// Set start and end day color
-								if (
-									// Check if the date is between
-									dayjs(
-										calendarDays.daysInMonth.value[j].date
-									).isBetween(
-										props.events![i].startDate,
-										props.events![i].endDate,
-										'date',
-										'[]'
-									)
-								) {
-									// Set color
-									calendarDays.daysInMonth.value[j].color =
-										props.events![i].color;
-								} else {
-								}
-							}
-						}
+            // Set events
+            for (let i = 0; i < props.events!.length; i++) {
+              // Check if date is between the start or end of the month
+              for (let j = 0; j < calendarDays.daysInMonth.value.length; j++) {
+                // Set start and end day color
+                if (
+                  // Check if the date is between
+                  dayjs(calendarDays.daysInMonth.value[j].date).isBetween(
+                    props.events![i].startDate,
+                    props.events![i].endDate,
+                    'date',
+                    '[]'
+                  )
+                ) {
+                  // Set color
+                  calendarDays.daysInMonth.value[j].color =
+                    props.events![i].color;
+                } else {
+                }
+              }
+            }
 
-						return true;
-					} catch (error) {
-						return false;
-					}
-				}
+            return true;
+          } catch (error) {
+            return false;
+          }
+        }
 
-				// Calling internal functions
-				await setDaysData();
-				await setDatesColor();
-				await setEmptyDays();
+        // Calling internal functions
+        await setDaysData();
+        await setDatesColor();
+        await setEmptyDays();
 
-				// Set month color
-				for (let i = 0; i < props.steps!.length; i++) {
-					if (
-						// Check if month is between the start or end of the step
-						dayjs(
-							`${props.year!}-${monthNum.value + 1}-${dayjs(props.steps![i].startDate).date()}`
-						).isBetween(
-							props.steps![i].startDate,
-							props.steps![i].endDate,
-							'date',
-							'[]'
-						) === true ||
-						dayjs(
-							`${props.year!}-${monthNum.value + 1}-${dayjs(props.steps![i].endDate).date()}`
-						).isBetween(
-							props.steps![i].startDate,
-							props.steps![i].endDate,
-							'date',
-							'[]'
-						) === true
-					) {
-						monthColor.value = props.steps![i].color;
-						break;
-					} else {
-						monthColor.value = '#9ab69e';
-					}
-				}
+        // Set month color
+        for (let i = 0; i < props.steps!.length; i++) {
+          if (
+            // Check if month is between the start or end of the step
+            dayjs(
+              `${props.year!}-${monthNum.value + 1}-${dayjs(props.steps![i].startDate).date()}`
+            ).isBetween(
+              props.steps![i].startDate,
+              props.steps![i].endDate,
+              'date',
+              '[]'
+            ) === true ||
+            dayjs(
+              `${props.year!}-${monthNum.value + 1}-${dayjs(props.steps![i].endDate).date()}`
+            ).isBetween(
+              props.steps![i].startDate,
+              props.steps![i].endDate,
+              'date',
+              '[]'
+            ) === true
+          ) {
+            monthColor.value = props.steps![i].color;
+            break;
+          } else {
+            monthColor.value = '#9ab69e';
+          }
+        }
 
-				return true;
-			} catch (error) {
-				return false;
-			}
-		}
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
 
-		// Calling internal functions
-		await setDays();
-		handleCalling(monthNum.value);
-	} catch (error) {}
+    // Calling internal functions
+    await setDays();
+    handleCalling(monthNum.value);
+  } catch (error) {}
 }
 
 // Toggle month
 async function toggleMonth(num: number): Promise<void> {
-	try {
-		// Toggle
-		monthNum.value += num;
+  try {
+    // Toggle
+    monthNum.value += num;
 
-		if (monthNum.value > 11) monthNum.value = 0;
-		else if (monthNum.value < 0) monthNum.value = 11;
+    if (monthNum.value > 11) monthNum.value = 0;
+    else if (monthNum.value < 0) monthNum.value = 11;
 
-		// Calling functions
-		await setMonth();
-		handleCalling(monthNum.value);
-	} catch (error) {}
+    // Calling functions
+    await setMonth();
+    handleCalling(monthNum.value);
+  } catch (error) {}
 }
 
 // --- Select week ---
@@ -255,159 +241,158 @@ const daysOfWeek = ref<dayjs.Dayjs[]>([]);
 
 // Get dates of week
 async function getWeek(date: dayjs.Dayjs): Promise<void> {
-	try {
-		// Check if select week is enable
-		if (props.selectWeek! === true) {
-			const startOfWeek = dayjs(date).startOf('week');
+  try {
+    // Check if select week is enable
+    if (props.selectWeek! === true) {
+      const startOfWeek = dayjs(date).startOf('week');
 
-			// Set dates
-			daysOfWeek.value = [];
-			for (let i = 1; i <= 6; i++) {
-				daysOfWeek.value.push(startOfWeek.add(i, 'day'));
-			}
-		}
-	} catch (error) {}
+      // Set dates
+      daysOfWeek.value = [];
+      for (let i = 1; i <= 6; i++) {
+        daysOfWeek.value.push(startOfWeek.add(i, 'day'));
+      }
+    }
+  } catch (error) {}
 }
 
 // Set hover
 async function hoverInWeek(
-	date: dayjs.Dayjs,
-	enableHover: boolean
+  date: dayjs.Dayjs,
+  enableHover: boolean
 ): Promise<void> {
-	try {
-		// Check if select week is enable
-		if (props.selectWeek! === true) {
-			// Set dates of week
-			await getWeek(date);
+  try {
+    // Check if select week is enable
+    if (props.selectWeek! === true) {
+      // Set dates of week
+      if (enableHover === true) await getWeek(date);
 
-			// Set hover in all days
-			for (let i = 0; i <= 6; i++) {
-				// Find day
-				const dayItem = calendarDays.daysInMonth.value.find(
-					(item) => item.date === daysOfWeek.value[i]
-				);
+      // Set hover in all days
+      if (daysOfWeek.value.length !== 0) {
+        for (let i = 0; i <= 6; i++) {
+          // Find day
+          const dayItem = calendarDays.daysInMonth.value.find(
+            (item) =>
+              item.date.format('MM-DD') === daysOfWeek.value[i].format('MM-DD')
+          );
 
-				// Active hover
-				if (dayItem) {
-					if (enableHover === true) dayItem!.hoverActive = true;
-					else dayItem!.hoverActive = false;
-				}
-			}
-		}
-	} catch (error) {}
+          // Active hover
+          if (dayItem) {
+            if (enableHover) dayItem!.hoverActive = true;
+            else dayItem!.hoverActive = false;
+          }
+        }
+      }
+    }
+  } catch (error) {}
 }
 
 onMounted(async () => {
-	// Calling functions
-	await setMonth();
+  // Calling functions
+  await setMonth();
 });
 </script>
 
 <template>
-	<v-card class="-month mx-auto rounded-lg w-[464px] h-[496px]">
-		<div
-			class="text-white flex justify-between items-center p-3 pl-6 pr-6 w-full"
-			:style="{ backgroundColor: monthColor }"
-		>
-			<!-- Toggle for before month -->
-			<IconsArrowIconArrow
-				class="text-white cursor-pointer"
-				@click="toggleMonth(-1)"
-				v-show="props.toggleMonth!"
-			/>
-			<!-- Month name -->
-			<h1 class="font-medium text-center text-xl w-full">
-				{{
-					dayjs(`${props.year!}-${monthNum + 1}-01`)
-						.format('MMMM')[0]
-						.toUpperCase() +
-					dayjs(`${props.year!}-${monthNum + 1}-01`)
-						.format('MMMM')
-						.slice(1)
-						.toLowerCase()
-				}}
-			</h1>
+  <v-card class="-month mx-auto rounded-lg w-[464px] h-[496px]">
+    <div
+      class="text-white flex justify-between items-center p-3 pl-6 pr-6 w-full"
+      :style="{ backgroundColor: monthColor }"
+    >
+      <!-- Toggle for before month -->
+      <IconsArrowIconArrow
+        class="text-white cursor-pointer"
+        @click="toggleMonth(-1)"
+        v-show="props.toggleMonth!"
+      />
+      <!-- Month name -->
+      <h1 class="font-medium text-center text-xl w-full">
+        {{
+          dayjs(`${props.year!}-${monthNum + 1}-01`)
+            .format('MMMM')[0]
+            .toUpperCase() +
+          dayjs(`${props.year!}-${monthNum + 1}-01`)
+            .format('MMMM')
+            .slice(1)
+            .toLowerCase()
+        }}
+      </h1>
 
-			<!-- Toggle for after month -->
-			<IconsArrowIconArrow
-				class="text-white rotate-180 cursor-pointer"
-				@click="toggleMonth(1)"
-				v-show="props.toggleMonth!"
-			/>
-		</div>
+      <!-- Toggle for after month -->
+      <IconsArrowIconArrow
+        class="text-white rotate-180 cursor-pointer"
+        @click="toggleMonth(1)"
+        v-show="props.toggleMonth!"
+      />
+    </div>
 
-		<!-- Calendar -->
-		<v-container
-			class="grid grid-cols-7 gap-4 justify-center items-center m-0"
-		>
-			<!-- Days of the week -->
-			<p
-				class="text-center font-semibold"
-				v-for="dayInTheWeek in daysInTheWeek"
-			>
-				{{ dayInTheWeek }}
-			</p>
+    <!-- Calendar -->
+    <v-container class="grid grid-cols-7 gap-4 justify-center items-center m-0">
+      <!-- Days of the week -->
+      <p
+        class="text-center font-semibold"
+        v-for="dayInTheWeek in daysInTheWeek"
+      >
+        {{ dayInTheWeek }}
+      </p>
 
-			<!-- Days -->
-			<!-- Before -->
-			<div
-				class="-empty-day w-12 h-12 rounded-lg bg-[#9ab69e]"
-				v-for="daysBefore in calendarDays.emptyDays.before.value"
-				:key="daysBefore"
-			></div>
+      <!-- Days -->
+      <!-- Before -->
+      <div
+        class="-empty-day w-12 h-12 rounded-lg bg-[#9ab69e]"
+        v-for="daysBefore in calendarDays.emptyDays.before.value"
+        :key="daysBefore"
+      ></div>
 
-			<!-- In month -->
-			<div
-				v-for="(dayInMonth, index) in calendarDays.daysInMonth.value"
-				:key="index"
-				class="w-12 h-12 flex border-solid rounded-lg"
-				:class="{ 'cursor-pointer': props.selectWeek }"
-				:style="{ backgroundColor: dayInMonth.color }"
-				@mouseenter="hoverInWeek(dayInMonth.date, true)"
-				@mouseleave="hoverInWeek(dayInMonth.date, false)"
-			>
-				<!-- Hover -->
-				<div
-					class="flex w-full h-full justify-center items-center"
-					:class="{
-						'-hover-off ': dayInMonth.hoverActive !== true,
-						'-hover-on ': dayInMonth.hoverActive === true,
-					}"
-				>
-					<p
-						:class="{
-							'border-2 border-solid border-white rounded-md':
-								dayjs(dayjs().toDate()).format('YYYY-MM-DD') ==
-								dayjs(dayjs(dayInMonth.date)).format(
-									'YYYY-MM-DD'
-								),
-						}"
-						class="mx-auto w-10 h-10 flex justify-center items-center text-center text-white font-semibold"
-					>
-						{{ dayjs(dayInMonth.date).format('D') }}
-					</p>
-				</div>
-			</div>
-			<!-- After -->
-			<div
-				class="-empty-day w-12 h-12 rounded-lg bg-[#9ab69e]"
-				v-for="daysAfter in calendarDays.emptyDays.after.value"
-				:key="daysAfter"
-			></div>
-		</v-container>
-	</v-card>
+      <!-- In month -->
+      <div
+        v-for="(dayInMonth, index) in calendarDays.daysInMonth.value"
+        :key="index"
+        class="w-12 h-12 flex border-solid rounded-lg"
+        :class="{ 'cursor-pointer': props.selectWeek }"
+        :style="{ backgroundColor: dayInMonth.color }"
+        @mouseenter="hoverInWeek(dayInMonth.date, true)"
+        @mouseleave="hoverInWeek(dayInMonth.date, false)"
+      >
+        <!-- Hover -->
+        <div
+          class="flex w-full h-full justify-center items-center"
+          :class="{
+            '-hover-off ': dayInMonth.hoverActive !== true,
+            '-hover-on ': dayInMonth.hoverActive === true,
+          }"
+        >
+          <p
+            :class="{
+              'border-2 border-solid border-white rounded-md':
+                dayjs(dayjs().toDate()).format('YYYY-MM-DD') ==
+                dayjs(dayjs(dayInMonth.date)).format('YYYY-MM-DD'),
+            }"
+            class="mx-auto w-10 h-10 flex justify-center items-center text-center text-white font-semibold"
+          >
+            {{ dayjs(dayInMonth.date).format('D') }}
+          </p>
+        </div>
+      </div>
+      <!-- After -->
+      <div
+        class="-empty-day w-12 h-12 rounded-lg bg-[#9ab69e]"
+        v-for="daysAfter in calendarDays.emptyDays.after.value"
+        :key="daysAfter"
+      ></div>
+    </v-container>
+  </v-card>
 </template>
 
 <style scoped>
 .-month {
-	border: solid 2px #9ab69e;
-	box-shadow: none;
-	margin: 0;
+  border: solid 2px #9ab69e;
+  box-shadow: none;
+  margin: 0;
 }
 .-hover-on {
-	background-color: #ffffff40;
+  background-color: #ffffff40;
 }
 .-hover-off {
-	background-color: #ffffff00;
+  background-color: #ffffff00;
 }
 </style>
