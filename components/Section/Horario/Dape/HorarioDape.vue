3f60308e32847efed3dc63682a24f1975ee3aeb6
Kauan <daluzmarkes@gmail.com>
2024-06-06 22:13:09 -0400

    feat: created the expansion panels of the Horario Dape page

--- components/Section/Horario/Dape/HorarioDape.vue
+++ components/Section/Horario/Dape/HorarioDape.vue
@@ -1,11 +1,81 @@
-<template>
-	<v-container class=" flex flex-col border-2 border-black pa-0 h-[130px] rounded-lg mt-5 items-center">
-		<div class="bg-green-500 h-3/6 w-full p-0 rounded-t-[6px]">
-			<v-title> Técnico em Informática </v-title>
-		</div>
+<script setup lang="ts">
+import type { IDiasDaSemana, ITurnos } from '../IGradeHorario';
 
-		<div class="flex justify-center justify-items-center">
-			<IconsArrowIconArrow class="rotate-[-90deg]"/>
-		</div>
-	</v-container>
+const diasDaSemana: IDiasDaSemana = [
+  { nome: 'Segunda' },
+  { nome: 'Terça' },
+  { nome: 'Quarta' },
+  { nome: 'Quinta' },
+  { nome: 'Sexta' },
+  { nome: 'Sábado' },
+];
+
+const turnos: ITurnos = [
+  {
+    nome: 'Matutino',
+    horarios: [
+      { hora: '07:30', tipo: 'aula' },
+      { hora: '08:20', tipo: 'aula' },
+      { hora: '09:10', tipo: 'aula' },
+      { hora: '10:00', tipo: 'intervalo' },
+      { hora: '10:20', tipo: 'aula' },
+      { hora: '11:10', tipo: 'aula' },
+    ],
+  },
+  {
+    nome: 'Vespertino',
+    horarios: [
+      { hora: '13:00', tipo: 'aula' },
+      { hora: '13:50', tipo: 'aula' },
+      { hora: '14:40', tipo: 'aula' },
+      { hora: '15:30', tipo: 'intervalo' },
+      { hora: '15:50', tipo: 'aula' },
+      { hora: '16:40', tipo: 'aula' },
+    ],
+  },
+  {
+    nome: 'Noturno',
+    horarios: [
+      { hora: '19:00', tipo: 'aula' },
+      { hora: '19:50', tipo: 'aula' },
+      { hora: '20:40', tipo: 'aula' },
+      { hora: '21:30', tipo: 'intervalo' },
+      { hora: '21:50', tipo: 'aula' },
+      { hora: '22:40', tipo: 'aula' },
+    ],
+  },
+];
+</script>
+
+<template>
+  <v-container>
+    <v-expansion-panels class="gap-3">
+      <v-expansion-panel
+        class="border-2 border-[#B2D2B7] pa-0 rounded-lg"
+        v-for="i in 4"
+      >
+        <v-expansion-panel-title class="!bg-[#118D3B] text-white">
+          Técnico em Informatica
+        </v-expansion-panel-title>
+        <v-expansion-panel-text class="overflow-auto">
+          <div class="grade-horario">
+            <SectionHorarioShifts
+              :turnos="turnos"
+              :dias-da-semana="diasDaSemana"
+            />
+          </div>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
 </template>
+<style scoped>
+.grade-horario {
+  display: grid;
+
+  column-gap: 16px;
+  grid-template-columns: min-content min-content repeat(6, 180px);
+
+  grid-auto-rows: auto;
+}
+</style>
