5deb998aea55326dbefc44bfa7e2489dbac13921
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-06-17 15:06:47 -0400

    feat(components/Section/Horario/DailyView/DaysAndLessons.vue): set class start times

--- components/Section/Horario/DailyView/DaysAndLessons.vue
+++ components/Section/Horario/DailyView/DaysAndLessons.vue
@@ -1,3 +1,19 @@
+<!-- // const lessons: ILesson[] = [
+// 	genericLesson,
+// 	genericLesson,
+// 	genericLesson,
+// 	genericLesson,
+// 	genericLesson,
+// ];
+
+// const activeIndex = 2;
+
+// function getVariantForIndex(index: number) {
+// 	if (index < activeIndex) return 'completed';
+// 	if (index === activeIndex) return 'active';
+// 	return;
+// } -->
+
 <script setup lang="ts">
 import type { ILesson } from '~/components/Section/Horario/-Helpers/ILesson';
 import dayjs from '../-Helpers/dayjs';
@@ -10,25 +26,11 @@ const genericLesson: ILesson = {
 	discipline: 'Linguagem de Programação',
 	class: '2° A Informática',
 	environment: 'Sala 20',
-	startsAt: '13:50',
-	endsAt: '14:40',
+	// startsAt: '13:50', //startsAt: start.format('HH.mm'),
+	// endsAt: '14:40', //endsAt: end.format('HH.mm'),
 };
 
-const lessons: ILesson[] = [
-	genericLesson,
-	genericLesson,
-	genericLesson,
-	genericLesson,
-	genericLesson,
-];
-
-const activeIndex = 2;
-
-function getVariantForIndex(index: number) {
-	if (index < activeIndex) return 'completed';
-	if (index === activeIndex) return 'active';
-	return;
-}
+const startClassTime = [ '7:30', '8:20', '9:10', '10:20', '11:10' ];
 </script>
 
 <template>
@@ -37,9 +39,9 @@ function getVariantForIndex(index: number) {
 
 		<div class="flex flex-col gap-5 mt-12">
 			<SectionHorarioDailyViewLesson
-				v-for="(lesson, index) in lessons"
-				:lesson="lesson"
-				:variant="getVariantForIndex(index)"
+				v-for="classTime in startClassTime"
+				:lesson="genericLesson"
+				:start-time="classTime"
 			/>
 		</div>
 	</div>
