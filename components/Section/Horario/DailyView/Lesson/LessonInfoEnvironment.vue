0299627de2fcb6bdac449830e77cb39cf6e00518
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-06-17 20:25:23 -0400

    feat(components/Section/Horario/DailyView/Lesson): add LessonInfoEnvironment.vue

---
+++ components/Section/Horario/DailyView/Lesson/LessonInfoEnvironment.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { ILesson } from '~/components/Section/Horario/-Helpers/ILesson';
+
+const lesson = inject<ILesson>("lesson")!
+
+</script>
+<template>
+  <p>Ambiente: {{ lesson.environment }}</p>
+</template>
