0fe19e3c3aab48769f640b01c797f4f01eac8eec
Sr-Iury <iury.silvafim@gmail.com>
2024-06-20 14:22:29 -0300

    feat: Organizing Journals Folders :sparkles:
    Partial organization of folders and scope of journals has been done

---
+++ components/Section/Diarios/Grid/Grid.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+	searchBarText: String,
+});
+
+const $emit = defineEmits(['edit']);
+
+const { searchBarText } = toRefs(props);
+
+const { cursos } = await useApiCursosFindAll(searchBarText);
+</script>
+
+<template>
+	<UIGrid :items="cursos">
+		<template #item="{ item: curso }">
+			<UICard
+				variant="block"
+				:src="`https://luna.sisgha.com/api/cursos/${curso.id}/imagem/capa?imgCapa=${curso.imagemCapa?.id}`"
+			>
+				<template #title>
+					{{ curso.nome }}
+				</template>
+
+				<template #actions>
+					<SectionCursosModal :editId="curso.id" />
+				</template>
+
+				<UICardLine>
+					<span>Abreviação: {{ curso.nomeAbreviado }}</span>
+				</UICardLine>
+				<UICardLine>
+					<span>Modalidade: {{ curso.modalidade.nome }}</span>
+				</UICardLine>
+			</UICard>
+		</template>
+	</UIGrid>
+</template>
