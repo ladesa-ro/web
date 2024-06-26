21c63e42babc75995250aca4f41a46ac6eb822de
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-06-26 04:23:49 -0400

    feat(components/Appbar/Appbar.vue): add ChangeJobTitle modal

--- components/Appbar/Appbar.vue
+++ components/Appbar/Appbar.vue
@@ -5,61 +5,92 @@ import { ref } from 'vue';
 import SpeechBubbles from './SpeechBubbles/SpeechBubbles.vue';
 
 const hamburgerActive = defineModel({
-		required: true,
-		type:Boolean,
-})
+  required: true,
+  type: Boolean,
+});
 
 function toggleHamburger() {
-	hamburgerActive.value = !hamburgerActive.value;
+  hamburgerActive.value = !hamburgerActive.value;
 }
 
 const notificationsButtonEl = ref(null);
 </script>
 
 <template>
-	<div class="flex w-full items-center h-full bg-white">
-		<div @click="toggleHamburger">
-			<IconsMenuIconHamburgerOn
-				class="pa-3 w-[45px]"
-				v-if="hamburgerActive"
-			/>
-			<IconsMenuIconHamburgerOff class="pa-3 w-[45px]" v-else />
-		</div>
+  <div class="flex w-full items-center h-full bg-white">
 
-		<div
-			class="flex items-center gap-3 cursor-pointer rounded-lg inset-y-0 w-29 bg-[#EBF8EF] pl-3 pr-6 py-2"
-		>
-			<VImg :width="48" :height="48" class="rounded-full bg-green-700" />
+    <!-- ========== menu icon configurations ========== -->
+    <div class="m-[0.34375rem]" @click="toggleHamburger"> <!--m-[5.5px]-->
+      <IconsMenuIconHamburgerOn class="pa-3 w-[45px]" v-if="hamburgerActive" />
+      <IconsMenuIconHamburgerOff class="pa-3 w-[45px]" v-else />
+    </div>
 
-			<div class="">
-				<p class="font-semibold">Danilo Escudero</p>
-				<p class="font-normal">DAPE</p>
-			</div>
-		</div>
+    <!-- =================== user ===================  -->
+    <div
+      class="flex items-center gap-3 cursor-pointer rounded-lg inset-y-0 w-29 bg-[#EBF8EF] pl-3 pr-6 py-2"
+    >
+      <VImg :width="48" :height="48" class="rounded-full bg-green-700" />
 
-		<div class="flex-1"></div>
+      <div>
+        <p class="font-semibold">Danilo Escudero</p>
+        <p class="font-normal flex flex-row">
+          DAPE
 
-		<v-menu
-			origin="auto"
-			location="bottom center"
-			transition="slide-y-transition"
-			:close-on-content-click="false"
-		>
-			<template v-slot:activator="{ props }">
-				<IconsIconNotifications
-					v-bind="props"
-					ref="notificationsButtonEl"
-					class="cursor-pointer pa-2 w-[38px] h-[38px]"
-				/>
-			</template>
+		  <!-- configuration to show the modal-->
+          <v-menu
+            origin="auto"
+            location="bottom center"
+            transition="slide-y-transition"
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ props, isActive }">
+              <IconsArrowIconArrow
+                v-bind="props"
+                ref="notificationsButtonEl"
+                :class="[
+                  'arrow -rotate-90 cursor-pointer -icon-green',
+                  { down: isActive },
+                ]"
+              />
+            </template>
 
-			<SpeechBubbles :notificationsButtonEl="notificationsButtonEl" />
-		</v-menu>
+			<!-- modal that will be shown if the arrow icon is clicked -->
+            <SectionUsuariosChangeJobTitle
+              :notificationsButtonEl="notificationsButtonEl"
+            />
+          </v-menu>
+        </p>
+      </div>
+    </div>
+    <!-- ============================================ -->
 
-		<div class="mr-3"></div>
+    <div class="flex-1"></div>
 
-		<LogoSisghaLogomarca
-			class="cursor-pointer mr-8 w-[140px] hidden sm:block"
-		/>
-	</div>
+	<!-- ============ notifications icon ============ -->
+    <v-menu
+      origin="auto"
+      location="bottom center"
+      transition="slide-y-transition"
+      :close-on-content-click="false"
+    >
+      <template v-slot:activator="{ props }">
+        <IconsIconNotifications
+          v-bind="props"
+          ref="notificationsButtonEl"
+          class="cursor-pointer pa-2 w-[38px] h-[38px]"
+        />
+      </template>
+
+	  <!-- modal that will be shown if the icon is clicked -->
+      <SpeechBubbles :notificationsButtonEl="notificationsButtonEl" />
+    </v-menu>
+    <!-- ============================================ -->
+
+    <div class="mr-3"></div>
+
+	<!-- SISGHA logo-->
+    <LogoSisghaLogomarca
+      class="cursor-pointer mr-8 w-[140px] hidden sm:block"
+    />
+  </div>
 </template>
