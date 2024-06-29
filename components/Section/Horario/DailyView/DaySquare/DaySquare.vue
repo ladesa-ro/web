bf98c02fef5c014b8e622298e55e1ee738b92eb4
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-06-28 22:41:09 -0400

    feat(DaySquare): scroll into view at first load
    Co-authored-by: Gabriel R. Antunes <gabrielrodantunes@gmail.com>

--- components/Section/Horario/DailyView/DaySquare/DaySquare.vue
+++ components/Section/Horario/DailyView/DaySquare/DaySquare.vue
@@ -4,33 +4,50 @@ type Props = {
   dayWeek: string;
   dayMonth: string;
 };
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const square = ref<HTMLDivElement>();
+
+const { active } = toRefs(props);
+
+watch(
+  [active, square],
+  ([active, square]) => {
+    if (active && square) {
+      square.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center',
+      });
+    }
+  },
+  {
+    once: true,
+  }
+);
 </script>
 
 <template>
-	<div
-		class="flex-1 flex flex-col cursor-pointer justify-center items-center rounded-[10px] border-2 border-[#118D3B] min-w-20
-			min-[900px]:p-4 min-[900px]:max-w-28 min-[900px]:max-h-28 min-[900px]:gap-2
-			max-[900px]:p-3 max-[900px]:max-w-20 max-[900px]:gap-1
-		"
-		:class="{ active: active }"
-	>
-		<p
-			class="border-b-2 border-b-[#118D3B] p-0 pb-[5px] font-[600] text-[#118D3B]
-				
-			"
-		>
-			{{ dayWeek }}
-		</p>
-		<p class="font-[600] text-[#118D3B]">
-			{{ dayMonth }}
-		</p>
-	</div>
+  <div
+    ref="square"
+    class="flex-1 flex flex-col cursor-pointer justify-center items-center rounded-[10px] border-2 border-[#118D3B] min-w-20 min-[900px]:p-4 min-[900px]:max-w-28 min-[900px]:max-h-28 min-[900px]:gap-2 max-[900px]:p-3 max-[900px]:max-w-20 max-[900px]:gap-1"
+    :class="{ active: active }"
+  >
+    <p
+      class="border-b-2 border-b-[#118D3B] p-0 pb-[5px] font-[600] text-[#118D3B]"
+    >
+      {{ dayWeek }}
+    </p>
+    <p class="font-[600] text-[#118D3B]">
+      {{ dayMonth }}
+    </p>
+  </div>
 </template>
 
 <style scoped>
 .active {
   background-color: #118d3b;
+  color: white;
 }
 
 div.active p {
--- components/Section/Horario/DailyView/DaySquare/DaySquareList.vue
+++ components/Section/Horario/DailyView/DaySquare/DaySquareList.vue
@@ -3,15 +3,44 @@ import { getWeekDays } from '~/components/Section/Horario/-Helpers/GetWeekDays';
 
 const selectedDay = useSelectedDay();
 
-const activeDayMonth = computed(() => selectedDay.value.format('DD/MM'));
+const activeDayMonth = computed(() => selectedDay.value.format('DD/MM')); //formata dia atual
 
-const weekDays = getWeekDays(selectedDay.value);
+const weekDays = getWeekDays(selectedDay.value); //array com dias da semana
+
+//===========================================================================================
+//vou precisar usar onMount pra executar assim que iniciar
+
+// const square = ref([]);
+
+// let i: number = 0;
+// for (const obj of weekDays) {
+//   i++;
+//   if (obj.dayMonth === activeDayMonth.value) break; //-> está aqui o que eu preciso
+// }
+
+//scrool
+
+// watch(
+//   [activeDayMonth],
+//   () => {
+//     if (typeof window !== 'undefined') {
+//       const activeDayElement = document.querySelector(
+//         `.scroll [data-day-month="${activeDayMonth.value}"]`
+//       );
+
+//       if (activeDayElement) {
+//         activeDayElement.scrollIntoView();
+//       }
+//     }
+//   },
+//   { immediate: true }
+// );
 </script>
 
 <template>
-  <div class="flex flex-row justify-between max-w-screen-2xl w-full mx-auto
-    max-sm:gap-3 max-sm:overflow-x-auto
-  ">
+  <div
+    class="flex flex-row justify-between max-w-screen-2xl w-full mx-auto max-sm:gap-3 max-sm:overflow-x-auto"
+  >
     <SectionHorarioDailyViewDaySquare
       v-for="weekDay in weekDays"
       :active="weekDay.dayMonth === activeDayMonth"
@@ -19,5 +48,7 @@ const weekDays = getWeekDays(selectedDay.value);
       :dayMonth="weekDay.dayMonth"
       @click="selectedDay = weekDay.dayAsDayJs"
     />
+
+    <!-- :data-day-month="weekDay.dayMonth"-->
   </div>
 </template>
