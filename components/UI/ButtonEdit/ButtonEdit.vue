a047f3edd86ee0be7f63dc1a06f8a77304f01ec2
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-09 12:53:56 -0400

    feat(UI/ButtonEdit): remove border

--- components/UI/ButtonEdit/ButtonEdit.vue
+++ components/UI/ButtonEdit/ButtonEdit.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="flex pa-2 justify-center items-center border-[1px] border-solid border-[#118D3B]/25 cursor-pointer rounded-lg transition-colors duration-150 hover:bg-[#118D3B]/10"
+    class="flex pa-2 justify-center items-center border-0 border-solid border-[#118D3B]/25 cursor-pointer rounded-lg transition-colors duration-150 hover:bg-[#118D3B]/10"
   >
     <IconsIconEdit class="text-black w-[42px] h-[42px]" />
   </button>
