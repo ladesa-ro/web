ba0d783a7c2b2762ce7f5345e83140a2f58a08ae
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-12 00:23:01 -0400

    feat(integrations): QuerySuspense

---
+++ integrations/-utils/QuerySuspense.ts
@@ -0,0 +1,49 @@
+import type { UseQueryReturnType } from '@tanstack/vue-query';
+import { wait } from './wait';
+
+export enum QuerySuspenseBehaviourMode {
+  AWALWAYS_WAIT,
+  NEVER_WAIT,
+  SILENT_WAIT,
+  AUTO,
+}
+
+export type QuerySuspenseBehaviour = {
+  mode?: QuerySuspenseBehaviourMode;
+};
+
+export const QuerySuspense = async (
+  query: UseQueryReturnType<any, any>,
+  suspenseBehaviour: QuerySuspenseBehaviour = {}
+) => {
+  const { mode = QuerySuspenseBehaviourMode.AUTO } = suspenseBehaviour;
+
+  const doSuspense = () => query.suspense().finally(() => {});
+
+  let consideredMode = mode;
+
+  if (consideredMode === QuerySuspenseBehaviourMode.AUTO) {
+    if (import.meta.server) {
+      consideredMode = QuerySuspenseBehaviourMode.AWALWAYS_WAIT;
+    } else {
+      consideredMode = QuerySuspenseBehaviourMode.SILENT_WAIT;
+    }
+  }
+
+  switch (consideredMode) {
+    case QuerySuspenseBehaviourMode.AWALWAYS_WAIT: {
+      await doSuspense();
+      break;
+    }
+
+    case QuerySuspenseBehaviourMode.SILENT_WAIT: {
+      await Promise.race([doSuspense(), wait(100)]);
+      break;
+    }
+
+    default:
+    case QuerySuspenseBehaviourMode.NEVER_WAIT: {
+      break;
+    }
+  }
+};
