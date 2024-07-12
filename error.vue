48a154ffb9c822e93c9cc3bdcc9cf6badf61ebfb
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-11 20:31:30 -0400

    feat(error): init component

---
+++ error.vue
@@ -0,0 +1,35 @@
+<script setup>
+const error = useError();
+</script>
+
+<template>
+  <NuxtLayout>
+    <template v-if="error.statusCode === 404">
+      <v-empty-state
+        class="empty-state flex-1"
+        headline="Oops! 404"
+        title="Página não encontrada"
+        text="A página que você está procurando não existe."
+      ></v-empty-state>
+    </template>
+
+    <template v-else>
+      <v-empty-state
+        class="empty-state flex-1"
+        headline="Oops!"
+        title="Houve um comportamento não esperado em nossa aplicação."
+      >
+        <div class="text-center">
+          <p>Pedimos desculpas por este inconveniente.</p>
+          <p>Mensagem do erro: {{ error.message }}</p>
+        </div>
+      </v-empty-state>
+    </template>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+.empty-state {
+  @apply min-h-min;
+}
+</style>
