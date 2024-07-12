1d188bace1cbb1e5183f63f1be1bc27742b2df11
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-12 01:55:16 -0400

    feat(components/VV/Autocomplete/Ambiente): show loading  state

--- components/VV/Autocomplete/Ambiente.vue
+++ components/VV/Autocomplete/Ambiente.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { toRefs } from 'vue';
+import { QuerySuspenseBehaviourMode } from '../../../integrations';
 
 const props = defineProps({
   name: {
@@ -12,11 +13,22 @@ const { name } = toRefs(props);
 
 const search = ref('');
 
-const { ambientesDebounced: ambientes } = await useApiAmbientesFindAll(search);
+const { previousItems: ambientes } = await useApiAmbientesFindAll(search, {
+  mode: QuerySuspenseBehaviourMode.NEVER_WAIT,
+});
 </script>
 
 <template>
   <VVAutocomplete
+    v-if="!ambientes"
+    :name="name"
+    label="Ambiente"
+    placeholder="Buscando por ambientes..."
+    disabled=""
+  />
+
+  <VVAutocomplete
+    v-else
     :name="name"
     item-value="id"
     label="Ambiente"
