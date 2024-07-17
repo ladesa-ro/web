539516ce87f3272bc1802912106c0cf2e626bba8
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-17 10:14:41 -0400

    refactor: init routes splitting for profiles

---
+++ pages/dape/reservas/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dape',
+});
+</script>
+
+<template>
+  <SectionReservas />
+</template>
---
+++ pages/professor/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'professor',
+});
+</script>
+
+<template>
+  <SectionHorario />
+</template>
