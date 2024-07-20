c9bc7324df3831da8c3feba0d8c2400723a7c311
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-19 22:01:17 -0400

    refactor(Section/Turmas/Form): use createFormContext

--- components/Section/Turmas/Form/Form.vue
+++ components/Section/Turmas/Form/Form.vue
@@ -1,19 +1,12 @@
 <script lang="ts" setup>
-import { type ModalidadeFindOneResultDto } from '@ladesa-ro/api-client-fetch';
-import { useQueryClient } from '@tanstack/vue-query';
-import { useForm } from 'vee-validate';
-import { computed, mergeProps } from 'vue';
-import * as yup from 'yup';
-import {
-  useApiClient,
-  useApiCursosFindOne,
-  useApiTurmasFindOne,
-} from '~/composables';
+import { createFormContext } from '../../../VV/FormContext/-Context/Context';
+import type { TurmaFormOutput, TurmaFormValues } from './-Helpers/typings';
+import { useTurmaHandleDelete } from './-Helpers/useTurmaHandleDelete';
+import { useTurmaHandleSubmit } from './-Helpers/useTurmaHandleSubmit';
 
 //
 
 const props = defineProps({
-  //props do modal criar e editar
   editId: {
     type: String,
     required: false,
@@ -21,365 +14,87 @@ const props = defineProps({
   },
 });
 
-const editIdRef = toRef(props, 'editId');
+const { editId } = toRefs(props);
 
 //
 
 const $emit = defineEmits(['close']);
+const onClose = () => $emit('close');
+
+//
+
+const { mutateAsync: turmaHandleDelete } = useTurmaHandleDelete();
+
+const handleDelete = async (id: string) => {
+  try {
+    const resposta = window.confirm(
+      'Você tem certeza de que deseja deletar esta turma?'
+    );
+
+    if (resposta) {
+      await turmaHandleDelete(id);
+      onClose();
+    }
+  } catch (e) {}
+};
+
+//
+
+const { mutateAsync: turmaHandleSubmit } = useTurmaHandleSubmit();
+
+const onSubmit = async (values: TurmaFormOutput) => {
+  await turmaHandleSubmit({ editId: unref(editId), values });
+  onClose();
+};
+
+//
 
 const apiClient = useApiClient();
-const queryClient = useQueryClient();
 
-const { turma: currentTurma } = await useApiTurmasFindOne(editIdRef);
+const existentDataRetrieverTurma = async (
+  id: string
+): Promise<TurmaFormValues> => {
+  const turma = await apiClient.turmas.turmaFindById({ id });
 
-type FormValues = {
-  imagem: Blob | null | undefined;
-
-  curso: {
-    id: string | null;
+  return {
+    imagem: null,
+    periodo: turma.periodo,
+    curso: { id: turma.curso.id ?? null },
+    ambientePadraoAula: { id: turma.ambientePadraoAula?.id ?? null },
   };
-  ambientePadraoAula: {
-    id: string | null;
-  };
-
-  periodo: string;
 };
+//
 
-type FormOutput = {
-  imagem: Blob | null | undefined;
-
-  curso: {
-    id: string;
-  };
-
-  ambientePadraoAula: {
-    id: string;
-  } | null;
-
-  periodo: string;
-};
-
-const schema = yup.object().shape({
-  imagem: yup.mixed().nullable().optional().default(null),
-
-  curso: yup.object().shape({
-    id: yup.string().required('Curso é obrigatório!').default(null),
-  }),
-
-  ambientePadraoAula: yup.object().shape({
-    id: yup.string().uuid().nullable().optional().default(null),
-  }),
-
-  periodo: yup.string().required('Período é obrigatório!').default(''),
-
-  // serie: yup.string().required('Série é obrigatório!'),
-  // letra: yup.string().required('Letra é obrigatório!'),
+const { formOnSubmit } = createFormContext<TurmaFormValues, TurmaFormOutput>({
+  editId,
+  onSubmit,
+  existentDataRetriever: existentDataRetrieverTurma,
 });
 
-const initialFormValues = reactive({
-  ...schema.cast(currentTurma.value ?? {}, {
-    stripUnknown: true,
-    assert: false,
-  }),
-});
-
-const handleDelete = async () => {
-  const resposta = window.confirm(
-    'Você tem certeza de que deseja deletar esta turma?'
-  );
-
-  if (resposta) {
-    await apiClient.turmas.turmaDeleteById({ id: editIdRef.value });
-    await queryClient.invalidateQueries({ queryKey: ['turmas'] });
-    $emit('close');
-  }
-};
-
-const {
-  resetForm,
-  handleSubmit,
-  setFieldValue,
-  values: formValues,
-} = useForm<FormValues, FormOutput>({
-  validationSchema: schema,
-  initialValues: initialFormValues,
-});
-
-const onSubmit = handleSubmit(async (values: FormOutput) => {
-  const editId = editIdRef.value;
-
-  const { imagem, ...data } = values;
-
-  let id;
-
-  if (editId === null) {
-    const turmaCriada = await apiClient.turmas.turmaCreate({
-      requestBody: { ...data },
-    });
-    id = turmaCriada.id;
-  } else {
-    await apiClient.turmas.turmaUpdateById({
-      id: editId,
-
-      requestBody: {
-        ...values,
-      },
-    });
-
-    id = editId;
-  }
-
-  if (imagem) {
-    await apiClient.turmas.turmaSetCoverImage({
-      id: id,
-      formData: { file: imagem },
-    });
-  }
-
-  await queryClient.invalidateQueries({
-    queryKey: ['turmas'],
-  });
-
-  resetForm();
-  $emit('close');
-}, console.error);
-
-const verificarModalidade = (
-  modalidade: ModalidadeFindOneResultDto
-): 'serie-turma' | 'periodo' | 'nao-implementado' => {
-  switch (modalidade.id) {
-    case '1f08fe79-8f99-493b-ade1-fe082b4761e1':
-    case 'aab71668-9dfc-46ae-8593-99dcb616a88d': {
-      return 'serie-turma';
-    }
-
-    case '3ec92df1-1c11-4990-8664-f17fbbd3ca41':
-    case '2fcfc6cb-8f79-44ff-9c06-96a6a955005b':
-    case 'c6079567-5975-4247-b8bc-892eeeeb1451': {
-      return 'periodo';
-    }
-
-    default: {
-      return 'nao-implementado';
-    }
-  }
-};
-
-const { curso: cursoSelecionado } = await useApiCursosFindOne(
-  computed(() => formValues?.curso?.id)
-);
-
-const estrategiaModalidade = computed(() => {
-  return (
-    cursoSelecionado.value &&
-    verificarModalidade(cursoSelecionado.value.modalidade)
-  );
-});
-
-const serie = computed({
-  get: () => formValues.periodo,
-  set: (value) => {
-    formValues.periodo = value;
-  },
-});
-
-const letra = computed({
-  get: () => formValues.periodo,
-  set: (value) => {
-    formValues.periodo = value;
-  },
-});
+//
 </script>
 
 <template>
-  <v-form @submit.prevent="onSubmit" class="form">
-    <div class="form-header grid grid-cols-1 items-center">
-      <h1
-        class="main-title justify-self-center row-start-1 col-start-1 col-span-1"
-      >
-        <span v-if="editId">Editar Turma</span>
-        <span v-else>Cadastrar Nova Turma</span>
-      </h1>
+  <form @submit.prevent="formOnSubmit">
+    <VVFormContextDialog
+      class="form"
+      :on-close="onClose"
+      :on-delete="handleDelete"
+      title-edit="Editar Turma"
+      title-create="Cadastrar Nova Turma"
+    >
+      <template #body>
+        <VVSelectImage name="imagem" />
 
-      <div
-        class="row-start-1 self-end justify-self-end col-start-1 col-span-1 flex items-center gap-2"
-      >
-        <v-menu
-          v-if="editId"
-          :close-on-content-click="true"
-          location="bottom end"
-        >
-          <template v-slot:activator="{ props: menu }">
-            <v-tooltip location="bottom">
-              <template v-slot:activator="{ props: tooltip }">
-                <v-btn
-                  icon="mdi-dots-vertical"
-                  variant="text"
-                  size="small"
-                  v-bind="mergeProps(menu, tooltip)"
-                >
-                </v-btn>
-              </template>
+        <VVAutocompleteCurso name="curso.id" />
 
-              <span>Opções</span>
-            </v-tooltip>
-          </template>
-
-          <v-list>
-            <v-list-item
-              slim
-              link
-              color="#e9001c"
-              density="compact"
-              @click.prevent="handleDelete"
-            >
-              <template v-slot:prepend>
-                <v-icon icon="mdi-delete"></v-icon>
-              </template>
-
-              <v-list-item-title>Deletar</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-
-        <v-btn
-          size="small"
-          type="button"
-          variant="text"
-          icon="mdi-close"
-          @click="$emit('close')"
+        <VVAutocompleteAmbiente
+          name="ambientePadraoAula.id"
+          label="Sala de Aula"
         />
-      </div>
-    </div>
 
-    <v-divider class="my-4" />
-
-    <div class="form-body modal-form">
-      <VVSelectImage name="imagem" />
-
-      <VVAutocompleteCurso name="curso.id" />
-
-      <VVAutocompleteAmbiente
-        name="ambientePadraoAula.id"
-        label="Sala de Aula"
-      />
-
-      <template v-if="cursoSelecionado">
-        <div
-          v-if="estrategiaModalidade === 'serie-turma'"
-          class="grid grid-cols-[2fr,1fr] gap-4"
-        >
-          <VVTextField
-            type="text"
-            name="serie"
-            label="Série"
-            v-model="serie"
-            placeholder="1°, 2°, 3°..."
-          />
-
-          <VVTextField
-            type="text"
-            name="letra"
-            label="Letra"
-            v-model="letra"
-            placeholder="A, B, C..."
-          />
-        </div>
-
-        <template v-else-if="estrategiaModalidade === 'periodo'">
-          <VVTextField
-            type="text"
-            name="periodo"
-            label="Período"
-            v-model="formValues.periodo"
-            placeholder="1° Período, 2° Período, 3° Período..."
-          />
-        </template>
-
-        <template v-else>
-          <v-alert type="warning">
-            O sistema ainda não suporta o cadastro de turmas para a modalidade
-            {{ cursoSelecionado.modalidade.nome }}.
-          </v-alert>
-        </template>
+        <SectionTurmasFormFieldsPeriodo />
       </template>
-    </div>
-
-    <v-divider />
-
-    <div class="form-footer button-group">
-      <UIButtonModalCancelButton @click="$emit('close')" />
-
-      <UIButtonModalEditButton v-if="editId" />
-      <UIButtonModalSaveButton v-else />
-    </div>
-  </v-form>
+    </VVFormContextDialog>
+  </form>
 </template>
-
-<style scoped>
-/* .form {
-	overflow: hidden;
-}
-
-.form-body {
-	overflow: auto;
-} */
-
-.form {
-  overflow: auto;
-}
-
-.modal-form {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-
-.main-title {
-  font-size: 24px;
-  font-weight: 700;
-}
-
-.form {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  padding: 32px;
-}
-
-.button-group {
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-
-  margin-top: 20px;
-  gap: 20px;
-}
-
-.button {
-  font-weight: 700;
-  margin-top: 20px;
-  cursor: pointer;
-  border: none;
-}
-
-.v-btn.buttonCancelar,
-.v-btn.buttonCadastro {
-  padding: 6px 20px;
-  border-radius: 8px;
-  height: auto;
-  text-transform: none;
-}
-
-@media screen and (max-width: 450px) {
-  .button-group {
-    flex-direction: column;
-    gap: 10px;
-  }
-
-  .v-btn.buttonCancelar,
-  .v-btn.buttonCadastro {
-    padding: 6px 20px;
-  }
-}
-</style>
