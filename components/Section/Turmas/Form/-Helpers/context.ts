1f2892d0186ec2b224319dcb32583d49fda3c339
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-07-19 21:18:17 -0400

    refactor(Section/Turmas/Form): verificar-modalidade

--- components/Section/Turmas/Form/-Helpers/context.ts
+++ components/Section/Turmas/Form/-Helpers/context.ts
@@ -1,6 +1,6 @@
 import { useForm } from 'vee-validate';
 import type { TurmaFormOutput, TurmaFormValues } from './typings';
 
-export const useTurmasFormVVContext = () => {
+export const useTurmaForm = () => {
   return useForm<TurmaFormValues, TurmaFormOutput>();
 };
