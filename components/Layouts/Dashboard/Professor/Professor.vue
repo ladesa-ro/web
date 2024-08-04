b19da0a511122e9dcc61dd56389ab657ef05cbe7
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-08-04 12:36:52 -0400

    refactor(components/Layouts/Dashboard/Base): common component for dape and professor

---
+++ components/Layouts/Dashboard/Professor/Professor.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { HeadTitleContext } from '../../../../infrastructure/HeadTitleContext';
+
+useTitle(HeadTitleContext.DASHBOARD);
+</script>
+
+<template>
+  <LayoutsDashboardBase>
+    <slot></slot>
+  </LayoutsDashboardBase>
+</template>
+
+<style scoped></style>
