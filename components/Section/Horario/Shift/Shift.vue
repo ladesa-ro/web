3d6062d2f3ba49d3c0664f80ed3699fce424c98b
Kauan Marques <daluzmarkes@gmail.com>
2024-10-18 21:04:41 -0400

    fix: class hover mouse on shift

--- components/Section/Horario/Shift/Shift.vue
+++ components/Section/Horario/Shift/Shift.vue
@@ -40,7 +40,7 @@ defineProps<Props>();
       <div
         v-for="(horario, index) in turno.horarios"
         :key="horario.hora"
-        class="text-center border-black m-0 px-2 flex items-center justify-center teste"
+        class="text-center border-black m-0 px-2 flex items-center justify-center hover:bg-[#EBF8EF]"
         :class="{
           'border-b-2 pb-[2px]': index < turno.horarios.length - 1,
           'mt-3': index === 0,
@@ -65,8 +65,5 @@ defineProps<Props>();
 .turno {
   writing-mode: vertical-lr;
   transform: rotate(180deg);
-}
-.teste:hover {
-  background: #EBF8EF;
 }
 </style>
