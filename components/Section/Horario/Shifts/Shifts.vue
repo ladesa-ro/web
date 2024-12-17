2b50ecb8e1d63f6e2be7e9177853d20028bf3486
Sr-Iury <iury.silvafim@gmail.com>
2024-12-16 23:24:49 -0300

    fix: teacher verification in 3 shifts :bug:
    The bug in the styling of the teacher verification in 3 shifts was corrected, now the verification is being done correctly and styled completely, with the supervision of PROFESSOR DANILO

--- components/Section/Horario/Shifts/Shifts.vue
+++ components/Section/Horario/Shifts/Shifts.vue
@@ -9,18 +9,15 @@ type Props = {
 
 defineProps<Props>();
 
-// Isso aqui é uma tabela que evidencia as disciplinas e professor, tem um erro Kauan, pq ele ta repetindo em todos os turnos, mais a verificaçaõ ta feita.
 const gradeDisciplinas = [
   { diaDaSemana: 1, horarios: [1], disciplina: 'Matemática', professor: 'Prof. João', turno: 'Matutino' },
   { diaDaSemana: 1, horarios: [2], disciplina: 'Física', professor: 'Prof. João', turno: 'Noturno' },
   { diaDaSemana: 1, horarios: [3], disciplina: 'Programação Web', professor: 'Prof. João', turno: 'Vespertino' },
 ];
 
-// Faz a verificaçaõ dos professores estão em três turnos simultâneamente, ta fazendo a verificaçao~certinho!
 const professoresEmTresTurnos = computed(() => {
   const turnosPorProfessor: Record<string, Set<string>> = {};
 
-  // Faz o mapeamento dos professores e turnos, um foreach paar auxiliar no resultado e evitar erro
   gradeDisciplinas.forEach(({ professor, turno }) => {
     if (!turnosPorProfessor[professor]) {
       turnosPorProfessor[professor] = new Set();
@@ -28,18 +25,20 @@ const professoresEmTresTurnos = computed(() => {
     turnosPorProfessor[professor].add(turno);
   });
 
-  // Retorna os professoresss que estõa em 3 turnos, mt, vp, e nt
   return Object.keys(turnosPorProfessor).filter(
     (professor) => turnosPorProfessor[professor].size === 3
   );
 });
 
-// Serve apenas para exibir na tela o alert de 3 professores em 3 turnos, foi pedido só o background mais tava mt simples, coloquei esse v-alert
 const showAlert = computed(() => professoresEmTresTurnos.value.length > 0);
+
+// Função para verificar se o professor está em 3 turnos
+const isProfessorInThreeShifts = (professor: string) => {
+  return professoresEmTresTurnos.value.includes(professor);
+};
 </script>
 
 <template>
-
   <SectionHorarioShift
     v-for="(turno, index) in turnos"
     :key="turno.nome"
@@ -48,13 +47,7 @@ const showAlert = computed(() => professoresEmTresTurnos.value.length > 0);
     :class="{
       'rounded-t-[12px]': index === 0,
       'rounded-b-[12px]': index === turnos.length - 1,
-    }"
-    :style="{
-      backgroundColor: gradeDisciplinas.some(
-        (d) => professoresEmTresTurnos.includes(d.professor) && d.turno === turno.nome
-      )
-        ? 'orange'
-        : 'inherit',
+      'border-l-4 border-orange-500': gradeDisciplinas.some(disciplina => isProfessorInThreeShifts(disciplina.professor) && disciplina.turno === turno.nome)
     }"
     :gradeDisciplinas="gradeDisciplinas"
   />
