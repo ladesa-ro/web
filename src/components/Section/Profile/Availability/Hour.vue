498e3743fbc98cc3ccffd53c946f715c555dd194
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-01-03 01:12:02 -0300

    feat: Hour component

---
+++ src/components/Section/Profile/Availability/Hour.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+type Props = {
+  initialHour: string;
+  endHour: string;
+}
+defineProps<Props>();
+
+const divider: string = ' - ';
+</script>
+
+<template>
+  <span class="font-[500]">
+    {{ initialHour }}
+    {{ divider }}
+    {{ endHour }}
+  </span>
+</template>
