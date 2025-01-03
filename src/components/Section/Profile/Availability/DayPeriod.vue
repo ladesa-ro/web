66fc8211687ef837efdd7595299b6926f2c4f460
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-01-03 01:12:18 -0300

    feat: DayPeriod component

---
+++ src/components/Section/Profile/Availability/DayPeriod.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+type Props = {
+  period: Object[] | any;
+  periodName: string;
+};
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <span class="periodName">
+      {{ periodName }}
+    </span>
+
+    <SectionProfileAvailabilityHour
+      v-for="hora in period"
+      :initial-hour="hora.initialHour"
+      :end-hour="hora.endHour"
+    />
+  </div>
+</template>
+
+<style scoped>
+.periodName {
+  @apply pb-2.5 mb-2.5 border-b-[2px] border-b-ldsa-grey text-center;
+}
+</style>
