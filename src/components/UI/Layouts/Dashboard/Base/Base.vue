0f8c15c65d4f82f85aca3480bf6f75af0929a827
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-01-14 01:49:40 -0300

    feat: grid to base layout

--- src/components/UI/Layouts/Dashboard/Base/Base.vue
+++ src/components/UI/Layouts/Dashboard/Base/Base.vue
@@ -2,48 +2,81 @@
 import { HeadTitleContext } from '~/infrastructure/HeadTitleContext';
 import { provideLayoutsDashboardContext } from './context';
 
+type Slots = {
+  sidebar(props: any): any;
+  default(props: any): any;
+};
+defineSlots<Slots>();
+
 //defines the tab name
 useTitle(HeadTitleContext.DASHBOARD);
 
 //defines if the hamburguer sidebar is active
 const { hamburguerActive } = provideLayoutsDashboardContext();
-
-//
-type Slots = {
-  sidebar(props: any): any;
-  default(props: any): any;
-};
-
-defineSlots<Slots>();
 </script>
 
 <template>
-  <div class="flex flex-col layout-height">
-    <!-- appbar -->
-    <header class="sticky">
-      <Appbar v-model="hamburguerActive" />
-    </header>
+  <div class="layout-grid layout-height">
+    <Appbar v-model="hamburguerActive" class="sticky" id="appbar" />
 
-    <div class="flex-1 flex flex-row overflow-hidden">
-      <!-- sidebar -->
-      <slot name="sidebar" />
+    <slot name="sidebar" id="sidebar" />
 
-      <main class="flex-1 flex flex-col overflow-auto">
-        <!-- main content -->
-        <section class="flex-1 flex flex-col"> <!--pt-14-->
-          <slot />
-        </section>
+    <main class="flex flex-col overflow-auto">
+      <!-- main content -->
+      <section
+        class="flex-1 flex flex-col"
+        :class="[
+          hamburguerActive ? 'content-sidebar-active' : 'content-default',
+        ]"
+      >
+        <slot />
+      </section>
 
-        <!-- footer -->
-        <UIFooter />
-      </main>
-    </div>
+      <UIFooter />
+    </main>
   </div>
 </template>
 
 <style scoped>
 .layout-height {
-  @apply h-[100vh]  /* fallback para caso o dispositivo não suporte dvh */
+  @apply h-[100vh]  /* fallback caso o dispositivo não suporte dvh */
         h-[100dvh];
+}
+
+/* Grid layout */
+
+.layout-grid {
+  @apply grid;
+  grid-template:
+    'appbar appbar' auto
+    'sidebar main' 1fr
+    / auto 1fr;
+}
+
+#appbar {
+  grid-area: appbar;
+}
+
+#sidebar {
+  grid-area: sidebar;
+}
+
+main {
+  grid-area: main;
+}
+
+/* Transition */
+
+.content-default {
+  @apply ml-14;
+}
+
+.content-sidebar-active {
+  @apply ml-64;
+}
+
+.content-sidebar-active,
+.content-default {
+  @apply transition-[margin] duration-[0.19s];
 }
 </style>
