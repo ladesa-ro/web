a89ad96a832808ec66745513e43f9c49dca574db
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-01-15 15:46:24 -0300

    feat: SidebarItemTemplate component

---
+++ src/components/Sidebar/SidebarItemTemplate.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+type Props = {
+  title: string;
+  icon: any;
+};
+defineProps<Props>();
+</script>
+
+<template>
+  <button class="base-template">
+    <img :src="icon" class="w-6 mr-4 -icon-white" alt="Ícone da funcionalidade." />
+    <span> {{ title }} </span>
+  </button>
+</template>
+
+<style scoped>
+.base-template {
+  @apply flex items-center;
+  @apply px-4 w-64 min-h-12;
+  @apply text-ldsa-white hover:bg-ldsa-white/[0.07];
+}
+</style>
