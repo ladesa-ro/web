8284f41aa8eb6330a6e9ab1e2641fb0c668da746
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-01-21 15:06:27 -0300

    feat: add tailwind to UiButton component

--- src/components/UI/Button/Button.vue
+++ src/components/UI/Button/Button.vue
@@ -1,49 +1,30 @@
 <template>
   <button class="ui-button">
-    <div v-show="$slots['start-icon']" class="start-icon">
+    <span v-show="$slots['start-icon']" class="start-icon">
       <slot name="start-icon" />
-    </div>
+    </span>
 
-    <div class="content">
+    <p class="text">
       <slot />
-    </div>
+    </p>
   </button>
 </template>
 
 <style scoped>
 .ui-button {
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-
-  width: 100%;
-
-  padding: 0.875rem 1.25rem;
-  border-radius: 0.5625rem;
-
-  @apply bg-ldsa-green-1 text-ldsa-white;
-
-  font-size: 1rem;
-  font-weight: 600;
-}
-
-.ui-button:disabled {
-  @apply bg-ldsa-green-1/50;
-  cursor: not-allowed;
-}
-
-.ui-button:focus {
-  outline: none;
+  @apply flex items-center w-full px-5 py-[0.875rem];
+  @apply rounded-[0.5625rem] bg-ldsa-green-1 font-semibold text-ldsa-white;
+  @apply disabled:bg-ldsa-green-1/50 disabled:cursor-not-allowed;
+  @apply focus:outline-none;
 }
 
 .ui-button:focus,
 .ui-button:focus-within {
   @apply border-ldsa-green-2/50;
-  @apply shadow-[0_0_0.5px_5px_rgba(var(--ladesa-green-1-color),0.4)]; /* box-shadow: 0px 0px 0.5px 5px #1b5b3f; */
+  @apply shadow-[0_0_0.5px_5px_rgba(var(--ladesa-green-1-color),0.3)];
 }
 
-.content {
-  flex: 1;
-  text-align: center;
+.text {
+  @apply flex-1 text-center;
 }
 </style>
