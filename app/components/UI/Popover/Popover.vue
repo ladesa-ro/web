88a90987e8e1880beeade3afffbdd78a7aca9f16
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-01-28 15:30:55 -0300

    feat: UiPopover component

---
+++ app/components/UI/Popover/Popover.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  PopoverArrow,
+  PopoverContent,
+  PopoverPortal,
+  PopoverRoot,
+  PopoverTrigger,
+} from 'radix-vue';
+
+type Props = {
+  popoverArrow?: boolean;
+};
+
+type Slots = {
+  activator: () => any;
+  default: () => any;
+};
+
+defineProps<Props>();
+defineSlots<Slots>();
+</script>
+
+<template>
+  <PopoverRoot>
+    <PopoverTrigger>
+      <slot name="activator" />
+    </PopoverTrigger>
+
+    <PopoverPortal>
+      <PopoverArrow v-if="popoverArrow" />
+
+      <PopoverContent>
+        <slot />
+      </PopoverContent>
+    </PopoverPortal>
+  </PopoverRoot>
+</template>
