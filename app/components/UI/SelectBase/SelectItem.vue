f34e31660e64193bfeec2508f70cd263b88052f5
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-03 21:17:28 -0300

    feat: SelectItem component

---
+++ app/components/UI/SelectBase/SelectItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { SelectItem, SelectItemText } from 'radix-vue';
+
+type Props = {
+  value: any;
+};
+const { value } = defineProps<Props>();
+</script>
+
+<template>
+  <SelectItem
+    :value="value"
+    :class="[
+      'flex items-center h-(--radix-select-trigger-height) px-3 font-medium cursor-pointer',
+      'transition-[background-color] duration-150 hover:bg-ldsa-green-2/10',
+    ]"
+  >
+    <SelectItemText>
+      {{ value }}
+    </SelectItemText>
+  </SelectItem>
+</template>
