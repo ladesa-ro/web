7db4545aa76176b183d83397a687172b665d21f5
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-14 14:18:11 -0400

    feat: footer icons paste in components/icons

---
+++ sisgha-sisgea/app/components/Icons/Footer/IconBook.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <title>book</title>
+    <path
+      d="M18,22A2,2 0 0,0 20,20V4C20,2.89 19.1,2 18,2H12V9L9.5,7.5L7,9V2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18Z"
+    />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  fill: currentColor;
+}
+</style>
