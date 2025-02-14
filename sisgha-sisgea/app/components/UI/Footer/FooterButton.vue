0c63f502bea5768bb147772e631784dfc2180165
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-14 14:17:35 -0400

    feat(components/UI/Footer/Button): href icon

--- sisgha-sisgea/app/components/UI/Footer/FooterButton.vue
+++ sisgha-sisgea/app/components/UI/Footer/FooterButton.vue
@@ -14,12 +14,25 @@ const { link } = defineProps<Props>();
 
 <template>
   <a
-    class="flex justify-strech items-center gap-2.5 rounded-sm py-1.5 px-3.5 text-sm font-medium tracking-[0.075em] transition-colors hover:bg-ldsa-grey/30 active:bg-ldsa-grey/60"
+    :class="[
+      'flex justify-strech items-center gap-2.5 rounded-sm px-3.5 py-2 text-sm font-medium tracking-[0.075em]',
+      'hover:bg-ldsa-grey/30 active:bg-ldsa-grey/60 transition-[background-color] duration-200',
+    ]"
     :key="link.href"
     :href="link.href"
   >
-    <component :is="link.icon" class="w-4 h-4" />
+    <component :is="link.icon" class="icon" />
 
     {{ link.label.toUpperCase() }}
+
+    <IconsFooterIconHRef class="icon mb-px" />
   </a>
 </template>
+
+<style scoped>
+@reference "~/assets/styles/app.css";
+
+.icon {
+  @apply w-[1.125rem] h-[1.125rem];
+}
+</style>
