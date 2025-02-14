0d79cd8ee8b4f616f2b6ccde3e621f3f8f08e82f
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-14 12:13:59 -0400

    feat: FooterButton component

---
+++ sisgha-sisgea/app/components/UI/Footer/FooterButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type Link = {
+  label: string;
+  icon: Component | (SVGElement & HTMLElement);
+  href: string;
+};
+
+type Props = {
+  link: Link;
+};
+
+const { link } = defineProps<Props>();
+</script>
+
+<template>
+  <a
+    class="flex justify-strech items-center gap-2.5 rounded-sm py-1.5 px-3.5 text-sm font-medium tracking-[0.075em] transition-colors hover:bg-ldsa-grey/30 active:bg-ldsa-grey/60"
+    :key="link.href"
+    :href="link.href"
+  >
+    <component :is="link.icon" class="w-4 h-4" />
+
+    {{ link.label.toUpperCase() }}
+  </a>
+</template>
