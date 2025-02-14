20b30dc94b4af8be6e3c242f91b7c8b123c9b4fc
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-14 14:32:30 -0400

    fix: footer max-height

--- sisgha-sisgea/app/components/UI/Footer/Footer.vue
+++ sisgha-sisgea/app/components/UI/Footer/Footer.vue
@@ -35,7 +35,7 @@ const footerLinks = [
 
 <template>
   <footer
-    class="flex-1 flex flex-col justify-stretch border-solid border-t border-t-ldsa-grey/20 bg-ldsa-grey/10"
+    class="flex-1 flex flex-col justify-stretch lg:max-h-[3.75rem] border-solid border-t border-t-ldsa-grey/20 bg-ldsa-grey/10"
   >
     <div
       class="flex flex-col-reverse lg:flex-row flex-wrap justify-center md:justify-start items-center gap-2 min-xl:px-24 max-lg:px-16 py-3 w-full max-w-[89%] mx-auto"
