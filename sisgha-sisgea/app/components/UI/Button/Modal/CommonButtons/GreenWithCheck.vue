0dba2e99bc8134251355cf81489e444788d0afe9
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-04-08 19:24:32 -0400

    feat(UI/Button/Modal/CommonButtons): GreenWithCheck component

---
+++ sisgha-sisgea/app/components/UI/Button/Modal/CommonButtons/GreenWithCheck.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type Props = { text: string };
+defineProps<Props>();
+</script>
+
+<template>
+  <UIButtonModalBaseLayout color="var(--ladesa-green-2-color)" type="submit">
+    {{ text }}
+    <template #icon>
+      <IconsIconConfirm />
+    </template>
+  </UIButtonModalBaseLayout>
+</template>
