47451c671130143b569748042b31c76a7b541a9e
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-04-08 19:24:58 -0400

    feat: use GreenWithCheck component in other files

--- sisgha-sisgea/app/components/UI/Button/Modal/FinishButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <UIButtonModalBaseLayout color="var(--ladesa-green-2-color)" type="submit">
-    Concluir
-    <template #icon>
-      <IconsIconConfirm />
-    </template>
-  </UIButtonModalBaseLayout>
-</template>
--- sisgha-sisgea/app/components/UI/Button/Modal/Ok.vue
+++ sisgha-sisgea/app/components/UI/Button/Modal/Ok.vue
@@ -1,8 +1,3 @@
 <template>
-  <UIButtonModalBaseLayout color="var(--ladesa-green-2-color)" type="submit">
-    Ok
-    <template #icon>
-      <IconsIconConfirm />
-    </template>
-  </UIButtonModalBaseLayout>
+  <UIButtonModalCommonButtonsGreenWithCheck text="Ok" />
 </template>
