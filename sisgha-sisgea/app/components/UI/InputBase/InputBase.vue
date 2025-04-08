cce630a858e99750132efc5f0f3e17bdd00f5125
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-04-08 14:15:34 -0400

    feat: v-models in inputbase

--- sisgha-sisgea/app/components/UI/InputBase/InputBase.vue
+++ sisgha-sisgea/app/components/UI/InputBase/InputBase.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { mergeProps } from 'vue';
 import type { CommonProps, SelectOrAutocompleteProps } from './CommonProps';
 import Autocomplete from './SelectOrAutocomplete/AutocompleteBase.vue';
 import Select from './SelectOrAutocomplete/SelectBase.vue';
 import TextField from './TextField/TextField.vue';
+
+//
 
 type Props = CommonProps &
   SelectOrAutocompleteProps & {
@@ -11,13 +14,38 @@ type Props = CommonProps &
 
 const props = defineProps<Props>();
 
-// TODO: adicionar v-model para lidar com o valor de textfield
+const attrs = useAttrs();
+
+const mergedProps = mergeProps(attrs, props);
+
+//
+
+  // model used for textfield's and autocomplete's text input
+const inputValue = defineModel<string>('inputText', {
+  required: false,
+  default: '',
+});
+
+  // model used just for select
+const selectItem = defineModel<any>('select', { required: false });
 </script>
 
 <template>
-  <TextField v-if="type === 'textfield' && !options" v-bind="props" />
+  <TextField
+    v-if="type === 'textfield' && !options"
+    v-bind="mergedProps"
+    v-model:inputText="inputValue"
+  />
 
-  <Select v-if="type === 'select' && options" v-bind="props" />
+  <Select
+    v-if="type === 'select' && options"
+    v-bind="mergedProps"
+    v-model:select="selectItem"
+  />
 
-  <Autocomplete v-if="type === 'autocomplete' && options" v-bind="props" />
+  <Autocomplete
+    v-if="type === 'autocomplete' && options"
+    v-bind="mergedProps"
+    v-model:inputText="inputValue"
+  />
 </template>
