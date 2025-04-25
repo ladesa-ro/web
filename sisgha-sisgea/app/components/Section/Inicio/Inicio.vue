add501a2f1b3457b64eecafdf6e0b633388a515a
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-04-25 10:11:40 -0400

    feat: add header :sparkles:

--- sisgha-sisgea/app/components/Section/Inicio/Inicio.vue
+++ sisgha-sisgea/app/components/Section/Inicio/Inicio.vue
@@ -1,5 +1,24 @@
-<template></template>
-
 <script lang="ts" setup>
 import { useRoute } from 'vue-router';
+import { ref } from 'vue';
 </script>
+
+<template>
+  <v-container>
+    <div class="container mx-auto max-w-[80%] p-10">
+      <div class="container-header">
+        <h1 class="main-title font-semibold">
+          Início - Acesso Rápido
+        </h1>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<style scoped>
+.main-title::before {
+  content: "";
+  border: 2px solid #1bba47;
+  margin-right: 0.5rem;
+}
+</style>
