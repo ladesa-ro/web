8173b53cf7d746be7e62920ca0844769b212cb66
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-04-26 13:03:21 -0400

    style: add margin-bottom to cards for better spacing :gem:

--- sisgha-sisgea/app/components/Section/Inicio/Cards/CardLink.vue
+++ sisgha-sisgea/app/components/Section/Inicio/Cards/CardLink.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="bg-green-50 border border-[color:#2F9E41] rounded-md p-6 flex flex-col items-center max-w-xs">
+  <div class="bg-green-50 border border-[color:#2F9E41] rounded-md p-6 flex flex-col items-center max-w-xs mb-5">
     <component :is="icon" class="w-16 h-16 mb-4" />
     <span class="text-green-600 text-[15px] font-bold">{{ title }}</span>
   </div>
