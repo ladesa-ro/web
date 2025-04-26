15df578f5b4da43d7e9cbd6f3655241f24c7c416
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-04-26 17:36:07 -0400

    perf: improve button organization :rocket:

--- sisgha-sisgea/app/components/Section/Inicio/Layout/Section.vue
+++ sisgha-sisgea/app/components/Section/Inicio/Layout/Section.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <div class="mb-5">
     <h2 class="font-medium mb-3 text-[18px]">{{ title }}</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="grid grid-cols-[repeat(auto-fill,_minmax(14rem,_1fr))] gap-4">
       <slot />
     </div>
     <hr class="divider flex-grow pt-5 border-t border-ldsa-grey" />
