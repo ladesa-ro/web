df0b527aeb20aa1e694043993f82ac7675d96477
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-04-27 11:50:07 -0400

    feat: implement navigation route :sparkles:

---
+++ sisgha-sisgea/app/components/Section/Relatorios/Relatorios.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto px-4">
+    <div class="mx-auto max-w-[80%] p-10">
+      <div class="container-header">
+        <h1 class="main-title font-semibold pb-5 text-[20px]">
+          Relatório de Aulas Ministradas
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
