43b1209ed965026fa0ed6e7fa0d399cacd3ce548
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-04-27 20:32:54 -0400

    feat: finnaly filters section :sparkles:

--- sisgha-sisgea/app/components/Section/Relatorios/Relatorios.vue
+++ sisgha-sisgea/app/components/Section/Relatorios/Relatorios.vue
@@ -1,14 +1,18 @@
-<script>
+<script setup lang="ts">
 import { ref } from 'vue';
 
-const professores = ['Danilo Pereira Escudero', 'Guilherme Ferreira', 'Lucas Silva'];
-const semestres = ['2024.1', '2024.2', '2023.1', '2023.2', '2022.1', '2022.2'];
+const professores = ref(['Danilo Pereira Escudero', 'Albanita', 'Leticia Pivetta']);
+const semestres = ref(['2024.1', '2024.2', '2023.1', '2023.2', '2022.1', '2022.2']);
+const bimestres = ref(['1º Bimestre', '2º Bimestre', '3º Bimestre', '4º Bimestre']);
+const disciplinas = ref(['Matemática', 'Português', 'História', 'Geografia']);
+const cursos = ['Técnico em Informática', 'Técnico em Química', 'Técnico em Florestas', ];
+const turmas = ['1º Ano', '2º Ano', '3º Ano'];
 
 </script>
 
 <template>
   <div class="container mx-auto px-4">
-    <div class="mx-auto max-w-[50%] p-10">
+    <div class="mx-auto max-w-[55%] p-10">
       <div class="container-header">
         <h1 class="main-title font-semibold pb-5 text-[20px]">
           Relatório de Aulas Ministradas
@@ -16,29 +20,63 @@ const semestres = ['2024.1', '2024.2', '2023.1', '2023.2', '2022.1', '2022.2'];
       </div>
       <div class="w-full">
         <!-- select professor -->
-        <div class="flex flex-col xl:flex-row w-full justify-between items-center m-auto mb-6">
+        <div class="grid grid-cols-1 xl:grid-cols-3 gap-4 w-full m-auto mb-6">
           <!-- professor -->
           <VVAutocomplete
             :items="professores"
-            class="xl:max-w-[22rem]"
+            class="w-full xl:col-span-2"
             label="Professor"
             name="professor.id"
-            placeholder="Selecione um professor"/>
+            placeholder="Selecione um professor" />
 
           <!-- semestre -->
           <VVAutocomplete
             :items="semestres"
-            class="xl:max-w-[10rem]"
+            class="w-full"
             label="Semestre"
             name="semestre.id"
-            placeholder="Selecione um semestre"/>
+            placeholder="Selecione um semestre" />
         </div>
+
 
         <!-- divisoria -->
          <div class="divisor relative">
           <div class="filters relative w-25 bg-ldsa-bg mx-[1.2rem] my-0 p-[0.4rem] text-[0.6rem] font-bold text-ldsa-grey z-2">Filtros Opcionais</div>
          </div>
 
+         <div class="grid grid-cols-1 xl:grid-cols-2 gap-4 w-full mt-4 m-auto mb-6">
+            <!-- bimestre -->
+            <VVAutocomplete
+              :items="bimestres"
+              class="w-full"
+              label="Bimestre"
+              name="bimestre.id"
+              placeholder="Selecione um bimestre" />
+
+            <!-- disciplina -->
+            <VVAutocomplete
+              :items="disciplinas"
+              class="w-full"
+              label="Disciplina"
+              name="disciplina.id"
+              placeholder="Selecione uma disciplina" />
+
+            <!-- cursos -->
+            <VVAutocomplete
+              :items="cursos"
+              class="w-full"
+              label="Curso"
+              name="curso.id"
+              placeholder="Selecione um curso" />
+
+            <!-- turmas -->
+            <VVAutocomplete
+              :items="turmas"
+              class="w-full"
+              label="Turma"
+              name="turma.id"
+              placeholder="Selecione uma turma" />
+          </div>
       </div>
     </div>
   </div>
