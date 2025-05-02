e0b80146269755de3636e2bb5650c369236a0d2b
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-05-02 19:27:02 -0400

    feat(UI/PopoverCalendar): init component

---
+++ sisgha-sisgea/app/components/UI/Popover/PopoverCalendar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import dayjs from 'dayjs';
+
+const open = defineModel<boolean>({ required: true, default: false });
+</script>
+
+<template>
+  <UIPopover popover-arrow v-model="open">
+    <template #activator>
+      <slot />
+    </template>
+
+    <div
+      class="bg-ldsa-bg border-ldsa-green-1 border-2 rounded-xl shadow-2xl"
+    >
+      <SectionCalendarioMonth
+        :year="dayjs().year()"
+        :month="dayjs().month()"
+        :events="[]"
+        :steps="[]"
+        :select-week="false"
+        :toggle-month="false"
+      />
+    </div>
+  </UIPopover>
+</template>
