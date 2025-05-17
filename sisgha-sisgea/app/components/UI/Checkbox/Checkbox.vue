42f95c906083e19904fd30f02bbd0cd21dec6ead
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-05-17 14:36:42 -0400

    feat: UICheckbox component

---
+++ sisgha-sisgea/app/components/UI/Checkbox/Checkbox.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {
+  CheckboxIndicator as Check,
+  CheckboxRoot as Checkbox,
+  CheckboxGroupRoot,
+} from 'reka-ui';
+
+type Props = { items: string[] };
+const { items } = defineProps<Props>();
+
+const checkedItems = defineModel<any>();
+</script>
+
+<template>
+  <CheckboxGroupRoot v-model="checkedItems">
+    <label v-for="item in items" class="flex items-center gap-1 cursor-pointer">
+      <span class="hover:bg-ldsa-green-2/10 content-box p-1.5 rounded-full">
+        <Checkbox
+          :value="item"
+          class="flex border-2 border-ldsa-grey rounded-sm w-5.5 h-5.5"
+        >
+          <Check class="flex-1 bg-ldsa-green-2 p-1 pt-1.5">
+            <IconsIconConfirm class="text-ldsa-white" />
+          </Check>
+        </Checkbox>
+      </span>
+
+      {{ item }}
+    </label>
+  </CheckboxGroupRoot>
+</template>
