7da20e8c70e7e76b137e35b2fab0746e57f6d1f2
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-06-01 00:37:51 -0400

    style: additional responsive tweaks :gem:

--- sisgha-sisgea/app/components/Section/Intervalos/Form/Form.vue
+++ sisgha-sisgea/app/components/Section/Intervalos/Form/Form.vue
@@ -27,21 +27,36 @@ const fim = computed({
 </script>
 
 <template>
-  <div class="flex flex-wrap gap-2 mt-3 pb-3 border-b border-ldsa-grey items-center">
-    <VVTextField v-model="inicio" type="time" label="Início" name="inicio" />
-    <VVTextField v-model="fim" type="time" label="Término" name="fim" />
+  <div
+    class="flex flex-col lg:flex-row items-center gap-2 mt-3 pb-3 border-b border-ldsa-grey"
+  >
+    <VVTextField
+      v-model="inicio"
+      type="time"
+      label="Início"
+      name="inicio"
+      class="flex-1 min-w-[110px] self-start"
+    />
+    <VVTextField
+      v-model="fim"
+      type="time"
+      label="Término"
+      name="fim"
+      class="flex-1 min-w-[110px] self-start"
+    />
     <button
       @click="handleConfirm"
-      class="bg-ldsa-green-1 px-2 py-3 rounded text-[0.75rem] text-ldsa-white"
+      class="bg-ldsa-green-1 px-2 py-3 rounded text-[0.75rem] text-ldsa-white self-center"
     >
-      <IconsIconConfirm class="flex w-6 h-6 mr-4" />
+      <IconsIconConfirm class="flex w-6 h-6" />
     </button>
     <button
       v-if="props.onCancel"
       @click="props.onCancel"
-      class="bg-gray-400 px-2 py-1 rounded text-[0.75rem] text-white"
+      class="bg-gray-400 px-2 py-1 rounded text-[0.75rem] text-white self-center"
     >
       Cancelar
     </button>
   </div>
 </template>
+
