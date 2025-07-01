eacd8436ca09bd9506ebbf70f9599c63fbbeaccb
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-07-01 19:34:35 -0400

    feat: add AppbarCampus component to display user campus information :sparkles:

---
+++ sisgha-sisgea/app/components/Appbar/AppBarCampus/AppbarCampus.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useCampusDoUsuario } from '~/composables/integrations/ladesa-api/modules/useCampusUsuario';
+
+const userId = '17ed5d7e-79d4-4cfd-811c-263247dc4511'; // ajuste se for dinâmico no futuro
+const { campus } = useCampusDoUsuario(userId);
+</script>
+
+<template>
+  <div
+    v-if="campus"
+    class="text-sm font-semibold text-ldsa-text-default mr-3 truncate max-w-[12rem]"
+  >
+    Campus: {{ campus }}
+  </div>
+</template>
