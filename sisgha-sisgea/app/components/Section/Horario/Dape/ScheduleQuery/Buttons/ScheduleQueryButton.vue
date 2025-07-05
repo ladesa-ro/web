d5f7825fac3ba75c3161da4268800a8b19e7ee9e
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-07-05 11:27:12 -0400

    fix(Section/Horario/Dape/ScheduleQuery/Buttons): button border width

--- sisgha-sisgea/app/components/Section/Horario/Dape/ScheduleQuery/Buttons/ScheduleQueryButton.vue
+++ sisgha-sisgea/app/components/Section/Horario/Dape/ScheduleQuery/Buttons/ScheduleQueryButton.vue
@@ -12,7 +12,7 @@ const buttonColor = { '--color': color };
 <template>
   <button
     :style="buttonColor"
-    class="flex items-center justify-center gap-1.5 min-w-10 h-10 px-2 border-3 rounded-lg transition-[background-color] border-(--color) text-(--color) hover:bg-(--color)/10 focus:outline-0 focus-visible:bg-(--color)/15 active:bg-(--color)/20 disabled:bg-transparent disabled:border-(--color)/40 disabled:text-(--color)/40"
+    class="flex items-center justify-center gap-1.5 min-w-10 h-10 px-2 border-2 rounded-lg transition-[background-color] border-(--color) text-(--color) hover:bg-(--color)/10 focus:outline-0 focus-visible:bg-(--color)/15 active:bg-(--color)/20 disabled:bg-transparent disabled:border-(--color)/40 disabled:text-(--color)/40"
   >
     <span v-if="text !== ''" class="font-semibold text-sm">{{ text }}</span>
     <span class="shrink-0" v-if="$slots['default']">
