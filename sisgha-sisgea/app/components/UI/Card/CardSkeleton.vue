8eb3826c040e6c6c7faabbd97cddf27660fe3a1a
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-07-06 18:42:53 -0400

    refactor: replace vuetify skeleton loader with custom implementation :hammer:

--- sisgha-sisgea/app/components/UI/Card/CardSkeleton.vue
+++ sisgha-sisgea/app/components/UI/Card/CardSkeleton.vue
@@ -1,6 +1,11 @@
 <template>
-  <!-- TODO: remove vuetify here -->
-  <v-skeleton-loader elevation="0" class="card" type="image, article" />
+  <div class="card animate-pulse">
+    <div class="h-40 bg-ldsa-grey rounded-md mb-4"></div>
+    <div class="space-y-2">
+      <div class="h-4 bg-ldsa-grey rounded w-3/4"></div>
+      <div class="h-4 bg-ldsa-grey rounded w-1/2"></div>
+    </div>
+  </div>
 </template>
 
 <style scoped src="./Card.css"></style>
