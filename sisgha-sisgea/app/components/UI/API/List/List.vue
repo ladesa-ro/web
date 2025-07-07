14ad771a9bc6a084af9df96d1f95bc6aa059d895
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-07-06 20:05:11 -0400

    refactor: streamline props and slots definitions in List.vue :hammer:

--- sisgha-sisgea/app/components/UI/API/List/List.vue
+++ sisgha-sisgea/app/components/UI/API/List/List.vue
@@ -12,28 +12,15 @@ import {
 } from './Context/UIApiListContext';
 import type { IGridItemSlotProps } from './Results/Grid/Typings/IGridItemSlotProps';
 
-//
-
-type Props = {
+const props = defineProps<{
   options: ICreateUIApiListContextOptions<Typings, CrudModule>;
-};
+}>();
 
-const props = defineProps<Props>();
-
-// TODO: retrieve from Typings
-type Item = any;
-
-type Slots = {
+const slots = defineSlots<{
   'options-actions'(props: any): any;
-
-  'grid-item'(props: IGridItemSlotProps<Item>): any;
-
+  'grid-item'(props: IGridItemSlotProps<any>): any;
   'grid-item-skeleton'(props: any): any;
-};
-
-const slots = defineSlots<Slots>();
-
-//
+}>();
 
 setupUIApiListContext(props.options);
 </script>
