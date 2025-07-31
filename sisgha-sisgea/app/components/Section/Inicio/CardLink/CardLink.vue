57b8e68b62cce811f4ea0d6e0dca03f5f944ae26
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-07-31 19:23:36 -0400

    feat(Section/Inicio): improve CardLink style

--- sisgha-sisgea/app/components/Section/Inicio/CardLink/CardLink.vue
+++ sisgha-sisgea/app/components/Section/Inicio/CardLink/CardLink.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 defineProps<{
-  to: string;
+  to?: string;
   icon: any;
   title: string;
 }>();
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <NuxtLink
     :to="to"
-    class="bg-ldsa-green-1/5 border border-ldsa-green-1/50 rounded-md p-6 flex flex-col items-center"
+    class="bg-ldsa-green-1/5 border border-ldsa-green-1/50 rounded-md p-6 flex flex-col items-center hover:brightness-110 hover:shadow-lg/5 hover:scale-102 transition-[background-color,scale,shadow,filter] duration-200 active:scale-98"
   >
     <component :is="icon" class="w-16 h-16 mb-4 text-ldsa-text-green" />
 
