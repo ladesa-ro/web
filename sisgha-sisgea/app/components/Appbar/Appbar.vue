23e8bed352e031a9eb5f8a8fc63de097ee83f3bc
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-08-14 01:09:31 -0400

    feat(Appbar): pointerdown in toggle button active

--- sisgha-sisgea/app/components/Appbar/Appbar.vue
+++ sisgha-sisgea/app/components/Appbar/Appbar.vue
@@ -4,17 +4,16 @@ import AppbarCampus from './AppBarCampus/AppbarCampus.vue';
 const hamburgerActive = defineModel<boolean>({
   required: true,
 });
-
-function toggleHamburger() {
-  hamburgerActive.value = !hamburgerActive.value;
-}
 </script>
 
 <template>
   <header
     class="flex items-center w-full max-[345px]:pr-1.5 pr-3 sm:pr-7 h-14 sm:h-[4.563rem] border-b border-ldsa-grey/50 bg-ldsa-bg"
   >
-    <button class="m-[0.34375rem] shrink-0" @click="toggleHamburger">
+    <button
+      class="m-[0.34375rem] shrink-0"
+      @pointerdown="hamburgerActive = !hamburgerActive"
+    >
       <IconsMenuHamburgerOn v-if="hamburgerActive" class="p-3 w-[2.813rem]" />
       <IconsMenuHamburgerOff v-else class="p-3 w-[2.813rem] h-full" />
     </button>
