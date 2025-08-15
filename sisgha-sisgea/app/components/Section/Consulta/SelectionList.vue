1f3d2a58e16e3248e04b16d3147f7b6feba4a8a8
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-08-15 17:03:25 -0400

    feat(Section/Consulta/SelectionList): init component

---
+++ sisgha-sisgea/app/components/Section/Consulta/SelectionList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const selectedOptions = ref({ formacao: null, curso: null});
+</script>
+
+<template>
+  {{ selectedOptions }}
+
+  <SectionConsultaAccordion
+    title="Formação"
+    :items="[
+      { value: 'formacao1', label: 'Técnico' },
+      { value: 'formacao2', label: 'Graduação' },
+      { value: 'formacao3', label: 'Pós-Graduação' },
+      { value: 'formacao4', label: 'Concomitante' },
+    ]"
+    v-model="selectedOptions.formacao"
+  />
+  <SectionConsultaAccordion
+    title="Curso"
+    :items="[
+      { value: 'curso1', label: 'Informática' },
+      { value: 'curso2', label: 'Química' },
+      { value: 'curso3', label: 'Floresta' },
+    ]"
+    v-model="selectedOptions.curso"
+  />
+</template>
