85459836634257a67a8da408a358722cadf1302c
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-08-25 16:39:21 -0400

    feat(composables/schedule/useScheduleTypes): improve types

--- sisgha-sisgea/app/composables/schedule/useScheduleTypes.ts
+++ sisgha-sisgea/app/composables/schedule/useScheduleTypes.ts
@@ -1,41 +1,58 @@
 import type { Dayjs } from 'dayjs';
 
+export type TempoDeAula = { horaInicio: string; horaFim: string };
+
+export type TemposDeAula = {
+  segunda: TempoDeAula[];
+  terca: TempoDeAula[];
+  quarta: TempoDeAula[];
+  quinta: TempoDeAula[];
+  sexta: TempoDeAula[];
+  sabado: TempoDeAula[];
+};
+
+//
+
 // intervalos de tempo recebidos pela api e editáveis pelo usuário
 export type HorarioEditavelType = 'aula' | 'vago';
 
 // intervalos de tempo automaticamente calculados por useCompleteSchedule()
 export type HorarioNaoEditavelType =
-| 'intervalo'
-| 'quebraTurnos'
-| 'transicaoDia';
+  | 'intervalo'
+  | 'quebraTurnos'
+  | 'transicaoDia';
 
 export type HorarioType = HorarioEditavelType | HorarioNaoEditavelType;
 
-export type HorarioEditavel = Horario & { tipo: HorarioEditavelType };
+export type HorarioEditavel = Horario & {
+  tipo: HorarioEditavelType;
+  diaSemana: string | number;
+  turnoId: string;
+};
 
 export type HorarioNaoEditavel = Horario & { tipo: HorarioNaoEditavelType };
 
 //
 
 type HorarioBase = {
-  id: number;
+  id: string;
   tipo: HorarioType;
   horaInicio: StringOrDayjs;
   horaFim: StringOrDayjs;
   data: StringOrDayjs;
-  turnoId: number;
 };
 
 export type Aula = HorarioBase & {
   tipo: 'aula';
-  disciplina: string;
-  professor: string;
-  turma: string;
+  ambiente: string;
+  diario: { turma: string; professor: string; disciplina: string };
 };
 
 export type Vago = HorarioBase & { tipo: 'vago' };
 
-export type Intervalo = HorarioBase & { tipo: 'intervalo' };
+export type Intervalo = HorarioBase & {
+  tipo: 'intervalo';
+};
 
 export type QuebraTurnos = HorarioBase & { tipo: 'quebraTurnos' };
 
@@ -47,13 +64,13 @@ export type Horario = Aula | Vago | Intervalo | QuebraTurnos | TransicaoDia;
 
 export type StringOrDayjs = string | Dayjs;
 
-export type HorarioDayjs = Horario & {
+export type HorDayjs = Horario & {
   data: Dayjs;
   horaInicio: Dayjs;
   horaFim: Dayjs;
 };
 
-export type HorarioString = Horario & {
+export type HorString = Horario & {
   data: string;
   horaInicio: string;
   horaFim: string;
