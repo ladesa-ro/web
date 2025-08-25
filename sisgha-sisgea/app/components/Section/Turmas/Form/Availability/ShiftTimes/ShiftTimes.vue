13331e484c0ff757a92db377260735518236050a
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-08-25 13:23:50 -0400

    feat: enable selecting all times in a shift in Turmas modal :sparkles:

--- sisgha-sisgea/app/components/Section/Turmas/Form/Availability/ShiftTimes/ShiftTimes.vue
+++ sisgha-sisgea/app/components/Section/Turmas/Form/Availability/ShiftTimes/ShiftTimes.vue
@@ -7,12 +7,39 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits(['update:selectedTimes']);
+
+const isAllSelected = (shift: { times: string[] }) => {
+  return shift.times.every(time => props.selectedTimes.includes(time));
+}
+
+const toggleAll = (shift: { times: string[] }) => {
+  const allSelected = isAllSelected(shift);
+  let newSelectedTimes = [...props.selectedTimes];
+
+  if (allSelected) {
+    newSelectedTimes = newSelectedTimes.filter(time => !shift.times.includes(time));
+  } else {
+    shift.times.forEach(time => {
+      if (!newSelectedTimes.includes(time)) newSelectedTimes.push(time);
+    });
+  }
+
+  emit('update:selectedTimes', newSelectedTimes);
+};
 </script>
 
 <template>
   <section class="flex justify-between gap-4">
     <div v-for="shift in dayShifts" :key="shift.title" class="flex-1">
-      <h1 class="font-medium mb-2">{{ capitalizeFirst(shift.title) }}</h1>
+      <div class="flex items-center pb-2 mb-2 border-b-1 border-ldsa-grey">
+        <UICheckbox
+          :items="['']"           
+          :model-value="isAllSelected(shift) ? [''] : []"
+          @update:modelValue="() => toggleAll(shift)"
+          class="mr-2 w-5 h-5"   
+        />
+        <h1 class="font-medium">{{ capitalizeFirst(shift.title) }}</h1>
+      </div>
       <UICheckbox
         v-model="props.selectedTimes"
         :items="shift.times"
