ffeb287dae74e2db89cd139246bc7a8a650c9a46
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-08-26 01:00:03 -0400

    feat: useCompleteSchedule convertions

--- sisgha-sisgea/app/composables/schedule/useCompleteSchedule.ts
+++ sisgha-sisgea/app/composables/schedule/useCompleteSchedule.ts
@@ -1,11 +1,18 @@
-import { aulasSemDiaSemanaExemplo, temposDeAulaExemplo } from './EXEMPLO';
 import {
   useClassDayWeek,
   useTempoDeAulaDayMonth,
 } from './useClassAndTempoDeAulaConverts';
 import { useFreePeriods } from './useFreePeriods';
 import { useNonTeachingPeriods } from './useNonTeachingPeriods';
-import type { Horario, HorDayjs } from './useScheduleTypes';
+import { convertDayJsToString } from './useScheduleConverts';
+import type {
+  Aula,
+  DiaEmTurnos,
+  Horario,
+  HorDayjs,
+  HorString,
+  TemposDeAula,
+} from './useScheduleTypes';
 import {
   useSeparateScheduleInDays,
   type Dias,
@@ -15,15 +22,24 @@ import { useSeparateScheduleInShifts } from './useSeparateScheduleInShifts';
 /**
  * Retorna o horário completo com aulas ordenadas, horas vagas, intervalos, quebras de turno e quebras de dia.
  */
-export const useHorarioCompleto = () => {
-  const aulas = aulasSemDiaSemanaExemplo.map(aula => useClassDayWeek(aula));
+export const useHorarioCompleto = (
+  temposDeAula: TemposDeAula,
+  aulas: Omit<Aula & HorString, 'diaSemana' | 'id'>[], // | Aula & HorString
+  turnosEDias: boolean = true,
+  mustBeDayjs: boolean = true
+  // onlyAulas: boolean = false TODO: completar implementação deste parâmetro
+) => {
+
+  // TODO: verificar se as aulas recebias como parametro sao do tipo Omit<Aula & HorString, 'diaSemana' | 'id'> ou do tipo Aula
+
+  const horarioAulas = aulas.map(aula => useClassDayWeek(aula));
 
   const temposDeAulaMap = useTempoDeAulaDayMonth(
-    aulas[0]!,
-    temposDeAulaExemplo
+    horarioAulas[0]!,
+    temposDeAula
   );
 
-  const aulasEVagos = useFreePeriods(temposDeAulaMap, aulas);
+  const aulasEVagos = useFreePeriods(temposDeAulaMap, horarioAulas);
 
   const aulasVagosEForaDoHorario = useNonTeachingPeriods(aulasEVagos);
 
@@ -46,7 +62,52 @@ export const useHorarioCompleto = () => {
         ])
       );
 
-  return horarioDiasETurnos;
+  // TODO: fazer conversão de apenas aulas de acordo com os parâmetros
+  
+  if (!mustBeDayjs) {
+    return returnAsString(
+      aulasVagosEForaDoHorario,
+      horarioDiasETurnos,
+      turnosEDias,
+      horarioTemUmDia
+    );
+  }
 
-  // TODO: fazer conversões de acordo com os parâmetros
+  return turnosEDias ? horarioDiasETurnos : aulasVagosEForaDoHorario;
+};
+
+// What to do if the return must be with string values
+const returnAsString = (
+  horarioTemposSemAula: (Horario & HorDayjs)[],
+  horarioDiasETurnos: DiaEmTurnos | Dias,
+  turnosEDias: boolean,
+  horarioTemUmDia: boolean
+) => {
+  if (!turnosEDias) {
+    const horarioTemposSemAulaString = horarioTemposSemAula.map(horario =>
+      convertDayJsToString(horario)
+    );
+
+    return horarioTemposSemAulaString;
+  }
+
+  const horarioDiasETurnosString = horarioTemUmDia
+    ? Object.entries(horarioDiasETurnos as DiaEmTurnos).map(([_, turno]) =>
+        Object.values(turno).map(horario => convertDayJsToString(horario))
+      )
+    : Object.fromEntries(
+        Object.entries(horarioDiasETurnos as Dias).map(([key, dia]) => [
+          key,
+          Object.fromEntries(
+            Object.entries(dia as DiaEmTurnos).map(([key, turno]) => [
+              key,
+              Object.entries(turno).map(([_, horario]) =>
+                convertDayJsToString(horario)
+              ),
+            ])
+          ),
+        ])
+      );
+
+  return horarioDiasETurnosString;
 };
