64c38d5968599f33206a7a198936aec7fe165c02
Ana Maria Brito Souza <anamariasouza701@gmail.com>
2025-09-21 13:16:45 -0400

    fix: correct alignment of input error message :bug:

--- sisgha-sisgea/app/components/UI/Form/TextField/TextField.vue
+++ sisgha-sisgea/app/components/UI/Form/TextField/TextField.vue
@@ -25,7 +25,7 @@ const emit = defineEmits(['update:modelValue', 'blur']);
     </label>
 
     <input
-      class="w-full h-full px-3 py-2 rounded-md disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none"
+      class="w-full h-full px-2 py-2 rounded-md disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none"
       :type="type"
       :placeholder="placeholder"
       :value="modelValue"
@@ -35,11 +35,11 @@ const emit = defineEmits(['update:modelValue', 'blur']);
       "
       @blur="emit('blur')"
     />
-    <div class="">
-      <p v-if="error" class="text-ldsa-red text-[11px] mb-1 px-1 text-left">
-        {{ error }}
-      </p>
-    </div>
+  </div>
+  <div class="w-full text-left -mt-5">
+    <p v-if="error" class="text-ldsa-red text-xs font-semibold">
+      {{ error }}
+    </p>
   </div>
 </template>
 
@@ -48,6 +48,5 @@ const emit = defineEmits(['update:modelValue', 'blur']);
 <style scoped>
 .input-base {
   padding: 0;
-  border-width: 1px;
 }
 </style>
