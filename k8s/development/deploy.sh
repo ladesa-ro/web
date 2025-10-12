c3070035052e560ab51c13088230aa56244d5230
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-10-11 20:15:52 -0400

    chore(k8s): replace API deployment files with web deployment files and add execution flags

--- k8s/development/deploy.sh
+++ k8s/development/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-kubectl apply -f ./deployments/api.yaml;
-kubectl apply -f ./ingress/api.yaml;
-kubectl apply -f ./services/api.yaml;
+set -xeo pipefail;
+
+kubectl apply -f ./deployments/web.yaml;
+kubectl apply -f ./ingress/web.yaml;
+kubectl apply -f ./services/web.yaml;
 
 kubectl rollout restart \
   deployment.apps/ladesa-ro-web \
