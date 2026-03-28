f20d8696c97272e05626ad652a0b8332438f4153
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-03-27 22:20:25 -0400

    chore: update deployment scripts and workflows for consistency
    - Introduced namespace and deployment variable usage in Kubernetes scripts.
    - Replaced `Dockerfile` with `.docker/Containerfile` in build context.
    - Updated GitHub workflow to reference new container file and refactored build configuration.

--- .deploy/development/deploy.sh
+++ .deploy/development/deploy.sh
@@ -2,11 +2,26 @@
 
 set -xeo pipefail;
 
-kubectl apply -f ./deployments/web.yaml;
-kubectl apply -f ./ingress/web.yaml;
-kubectl apply -f ./services/web.yaml;
+K8S_NAMESPACE=ladesa-ro-development
+K8S_DEPLOYMENT=ladesa-ro-web
+
+kubectl apply \
+  --namespace ${K8S_NAMESPACE} \
+  -f ./deployments/web.yaml;
+
+kubectl apply \
+  --namespace ${K8S_NAMESPACE} \
+  -f ./services/web.yaml;
+
+kubectl apply \
+  --namespace ${K8S_NAMESPACE} \
+  -f ./ingress/web.yaml;
 
 kubectl rollout restart \
-  deployment.apps/ladesa-ro-web \
-  --namespace ladesa-ro-development \
-;
+  deployment.apps/${K8S_DEPLOYMENT} \
+  --namespace ${K8S_NAMESPACE};
+
+kubectl rollout status \
+  deployment.apps/${K8S_DEPLOYMENT} \
+  --namespace ${K8S_NAMESPACE} \
+  --timeout=720s;
