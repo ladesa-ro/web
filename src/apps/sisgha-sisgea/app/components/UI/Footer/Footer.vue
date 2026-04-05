0624a450e39b413c2deec401fe9d4b2729155386
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-04-05 12:34:05 -0400

    fix: grow-0 on footer

--- src/apps/sisgha-sisgea/app/components/UI/Footer/Footer.vue
+++ src/apps/sisgha-sisgea/app/components/UI/Footer/Footer.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { useNow } from '@vueuse/core';
 import { IconsFooterBook, IconsFooterGitHub } from '#components';
+import { useNow } from '@vueuse/core';
 
 const now = useNow();
 
@@ -34,7 +34,7 @@ const footerLinks = [
 
 <template>
   <footer
-    class="flex-1 flex flex-col justify-stretch lg:max-h-[3.75rem] border-solid border-t border-t-ldsa-grey/20 bg-ldsa-grey/10"
+    class="flex-1 grow-0 flex flex-col justify-stretch lg:max-h-[3.75rem] border-solid border-t border-t-ldsa-grey/20 bg-ldsa-grey/10"
   >
     <div
       class="flex flex-col-reverse lg:flex-row flex-wrap justify-center md:justify-start items-center gap-2 min-xl:px-24 max-lg:px-16 py-3 w-full max-w-[89%] mx-auto"
