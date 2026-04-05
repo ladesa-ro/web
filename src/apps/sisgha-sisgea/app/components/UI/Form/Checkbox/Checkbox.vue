5316cd185c8be641c86f74bc15c154ba2666ad71
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-04-05 16:13:34 -0400

    feat: checkbox form

--- src/apps/sisgha-sisgea/app/components/UI/Form/Checkbox/Checkbox.vue
+++ src/apps/sisgha-sisgea/app/components/UI/Form/Checkbox/Checkbox.vue
@@ -1,30 +1,29 @@
 <script lang="ts" setup>
 import { CheckboxIndicator as Check, CheckboxRoot as Checkbox } from 'reka-ui';
-import { useField } from 'vee-validate';
 
 const props = defineProps<{
-  name: string;
+  modelValue: boolean;
   label?: string;
   disabled?: boolean;
 }>();
 
-const name = computed(() => props.name);
+const emit = defineEmits<{
+  'update:modelValue': [value: boolean];
+}>();
 
-const { value: fieldValue } = useField<boolean>(name);
+const checked = computed({
+  get: () => props.modelValue,
+  set: (val: boolean) => emit('update:modelValue', val),
+});
 </script>
 
 <template>
-  <label
-    class="flex items-center gap-2.5 cursor-pointer"
-    :class="{ 'opacity-60 cursor-not-allowed': disabled }"
-  >
-    <span
-      class="rounded-full checkbox-shadow focus-within:shadow-(--green-shadow) hover:shadow-(--green-shadow)"
-    >
+  <label class="flex items-center gap-2.5 cursor-pointer" :class="{ 'opacity-60 cursor-not-allowed': disabled }">
+    <span class="rounded-full checkbox-shadow focus-within:shadow-(--green-shadow) hover:shadow-(--green-shadow)">
       <Checkbox
-        v-model:model-value="fieldValue"
+        v-model:model-value="checked"
         :disabled="disabled"
-        :class="fieldValue ? 'border-ldsa-green-2' : 'border-ldsa-grey'"
+        :class="checked ? 'border-ldsa-green-2' : 'border-ldsa-grey'"
         class="flex border-2 hover:bg-ldsa-green-2/10 rounded-sm w-5.5 h-5.5 focus-visible:outline-ldsa-green-2"
       >
         <Check class="flex-1 bg-ldsa-green-2 p-1 pt-1.5">
@@ -32,11 +31,8 @@ const { value: fieldValue } = useField<boolean>(name);
         </Check>
       </Checkbox>
     </span>
-    <span
-      v-if="label"
-      class="text-sm font-medium text-ldsa-text-default select-none"
-    >
-      {{ label }}
+    <span v-if="label || $slots.default" class="text-sm font-medium text-ldsa-text-default select-none">
+      <slot>{{ label }}</slot>
     </span>
   </label>
 </template>
@@ -46,7 +42,6 @@ const { value: fieldValue } = useField<boolean>(name);
   --green: rgb(from var(--ladesa-green-2-color) R G B / 10%);
   --green-shadow: 0 0 0 0.35rem var(--green);
 }
-
 .checkbox-shadow:focus-within {
   background-color: var(--green);
 }
