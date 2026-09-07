a906975552cec61f905173c52b2e4b75dc82093f
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-09-07 11:17:18 -0300

    feat(ui): create web.ui component package with Button and icons

---
+++ src/packages/ui/src/icons/Arrow/ArrowAlt.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg fill="none" viewBox="0 0 122 125" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M62.1102 13L13 62.5M62.1102 112L13 62.5M109.39 62.5H13"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="25"
+    />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  stroke: currentColor;
+}
+</style>
---
+++ src/packages/ui/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [vue()],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+    },
+  },
+});
